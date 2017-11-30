c98c09ba44f4673eece4ed69b84c96e5edf58fed
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2017-11-30 13:06:55 +0100

    Introduce superadmin role (#1)
    * Introduce superadmin role
    
    * Removed unused backend logic

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/enums/UserRole.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/enums/UserRole.java
@@ -1,8 +1,5 @@
 package com.liberologico.cloudesire.cmw.model.enums;
 
-import java.util.Arrays;
-import java.util.Set;
-
 /**
  * Defines user roles across the applications.
  *
@@ -10,84 +7,17 @@ import java.util.Set;
  */
 public enum UserRole
 {
-    ROLE_ANONYMOUS
-    {
-        @Override
-        public boolean canCreate( UserRole userRole )
-        {
-            return userRole.equals( ROLE_USER ) || userRole.equals( ROLE_VENDOR );
-        }
-    },
+    ROLE_ANONYMOUS,
     ROLE_USER,
-    ROLE_VENDOR
-    {
-        @Override
-        public boolean canCreate( UserRole userRole )
-        {
-            return userRole.equals( ROLE_VENDOR );
-        }
-    },
-    ROLE_RESELLER
-    {
-        @Override
-        public boolean canCreate( UserRole userRole )
-        {
-            return userRole.equals( ROLE_USER );
-        }
-    },
-    ROLE_DISTRIBUTOR
-    {
-        @Override
-        public boolean canCreate( UserRole userRole )
-        {
-            return userRole.equals( ROLE_RESELLER );
-        }
-    },
-    ROLE_SUPERVISOR
-    {
-        @Override
-        public boolean canCreate( UserRole userRole )
-        {
-            return ! userRole.equals( ROLE_ADMIN );
-        }
-    },
-    ROLE_ADMIN
-    {
-        @Override
-        public boolean canCreate( UserRole userRole )
-        {
-            return true;
-        }
-    };
+    ROLE_VENDOR,
+    ROLE_RESELLER,
+    ROLE_DISTRIBUTOR,
+    ROLE_SUPERVISOR,
+    ROLE_ADMIN,
+    ROLE_SUPERADMIN;
 
     public String prettyPrint()
     {
         return this.toString().replace( "ROLE_", "" ).toLowerCase();
-    }
-
-    @SuppressWarnings( "squid:S1172" )
-    public boolean canCreate( UserRole userRole )
-    {
-        return false;
-    }
-
-    public boolean canImpersonate()
-    {
-        return this.compareTo( ROLE_SUPERVISOR ) >= 0;
-    }
-
-    public boolean isSuperior( Set<UserRole> roles )
-    {
-        for ( UserRole role : roles )
-        {
-            if ( this.compareTo( role ) < 0 ) return false;
-        }
-
-        return true;
-    }
-
-    public UserRole[] lesserRoles()
-    {
-        return Arrays.copyOfRange( UserRole.values(), 0, this.ordinal() );
     }
 }
