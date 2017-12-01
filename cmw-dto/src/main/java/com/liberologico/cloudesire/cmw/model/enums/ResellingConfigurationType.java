5a96cb508b2c63813bd9d025f1559a724a52a3f4
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2017-12-01 15:53:36 +0100

    Clarify ResellingConfigurationType types

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/enums/ResellingConfigurationType.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/enums/ResellingConfigurationType.java
@@ -4,9 +4,9 @@ import io.swagger.annotations.ApiModelProperty;
 
 public enum ResellingConfigurationType
 {
-    @ApiModelProperty( "The value is a percentage on the original price" )
+    @ApiModelProperty( "The value is a percentage on the previous price of the selling chain" )
     PERCENTAGE,
 
-    @ApiModelProperty( "The value is a new price" )
+    @ApiModelProperty( "The value is a price in the actual currency" )
     VALUE
 }
