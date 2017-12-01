fe6ceedc55fc6a3fe524e90e5d113b95d5a38755
Matteo Giordano <matteo.giordano@cloudesire.com>
2017-12-01 12:50:10 +0100

    Add reselling configuration type

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/ResellingPriceDTO.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/ResellingPriceDTO.java
@@ -1,5 +1,6 @@
 package com.liberologico.cloudesire.cmw.model.dto;
 
+import com.liberologico.cloudesire.cmw.model.enums.ResellingConfigurationType;
 import io.swagger.annotations.ApiModelProperty;
 
 import java.math.BigDecimal;
@@ -10,11 +11,15 @@ public class ResellingPriceDTO extends DTO
     @ApiModelProperty( "The markup percentage on the product price, editable by the distributor" )
     private BigDecimal markup;
 
+    private ResellingConfigurationType markupType;
+
     @ApiModelProperty( value = "The sell-in price of this product", readOnly = true )
     private BigDecimal sellin;
 
     @ApiModelProperty( "The sell-out price for this product, editable by a reseller" )
     private BigDecimal sellout;
+
+    private ResellingConfigurationType selloutType;
 
     // region Auto-generated code
     public ResellingPriceDTO( BigDecimal markup, BigDecimal sellout )
@@ -42,6 +47,16 @@ public class ResellingPriceDTO extends DTO
         this.markup = markup;
     }
 
+    public ResellingConfigurationType getMarkupType()
+    {
+        return markupType;
+    }
+
+    public void setMarkupType( ResellingConfigurationType markupType )
+    {
+        this.markupType = markupType;
+    }
+
     public BigDecimal getSellin()
     {
         return sellin;
@@ -62,19 +77,30 @@ public class ResellingPriceDTO extends DTO
         this.sellout = sellout;
     }
 
+    public ResellingConfigurationType getSelloutType()
+    {
+        return selloutType;
+    }
+
+    public void setSelloutType( ResellingConfigurationType selloutType )
+    {
+        this.selloutType = selloutType;
+    }
+
     @Override
     public boolean equals( Object o )
     {
         if ( this == o ) return true;
         if ( ! ( o instanceof ResellingPriceDTO ) ) return false;
         ResellingPriceDTO that = (ResellingPriceDTO) o;
-        return Objects.equals( markup, that.markup ) && Objects.equals( sellout, that.sellout );
+        return Objects.equals( markup, that.markup ) && markupType == that.markupType && Objects
+                .equals( sellout, that.sellout ) && selloutType == that.selloutType;
     }
 
     @Override
     public int hashCode()
     {
-        return Objects.hash( markup, sellout );
+        return Objects.hash( markup, markupType, sellout, selloutType );
     }
     // endregion
 }
