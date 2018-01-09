d4303adb7c98e18ecf6d2f6616c5530b937d9683
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-01-09 12:37:37 +0100

    Add reseller characters not permitted error message

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/utils/ConstraintKeys.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/utils/ConstraintKeys.java
@@ -13,6 +13,7 @@ public final class ConstraintKeys
     public static final String THREE_OR_FOUR_DIGITS = "{must.contain.three.or.four.digits.%element%}";
     public static final String ALPHANUMERIC_UNDERSCORE = "{must.be.alphanumeric.underscore.is.permitted.%element%}";
     public static final String CHARACTERS_NOT_PERMITTED = "{characters.not.permitted.%element%}";
+    public static final String CHARACTERS_NOT_PERMITTED_RESELLER = "{characters.not.permitted.reseller.%element%}";
     public static final String COMPANY_NOT_ALLOWED = "{company.not.allowed.%element%}";
     public static final String FEATURE_DISABLED = "{feature.disabled.%element%}";
 
