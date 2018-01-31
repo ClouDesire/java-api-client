25611e0ef6a5e398acc8aee67c33c52294e69522
Simone Locci <simone.locci@cloudesire.com>
2018-01-31 12:04:11 +0100

    Add ApiModelProperty

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/enums/Trial.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/enums/Trial.java
@@ -1,8 +1,15 @@
 package com.liberologico.cloudesire.cmw.model.enums;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public enum Trial
 {
+    @ApiModelProperty( "The trial is not permitted" )
     NOT_ALLOWED,
+
+    @ApiModelProperty( "The trial is permitted but a payment method will be asked" )
     ALLOWED,
+
+    @ApiModelProperty( "The trial is permitted without asking for a payment method" )
     ALLOWED_WO_PAYMENT_METHOD
 }
