7df33577b97dee622496df5c8de882f0310d634c
Simone Locci <simone.locci@cloudesire.com>
2018-02-09 09:43:59 +0100

    Wip

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CouponApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CouponApi.java
@@ -2,7 +2,6 @@ package com.cloudesire.platform.apiclient.api;
 
 import com.cloudesire.platform.apiclient.DateWrapper;
 import com.liberologico.cloudesire.cmw.model.dto.CouponDTO;
-import com.liberologico.cloudesire.cmw.model.enums.CouponType;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -25,7 +24,7 @@ public interface CouponApi
     Call<Void> delete( @Path( "id" ) Integer id );
 
     @POST( "coupon" )
-    Call<CouponDTO> create( @Query( "type" ) CouponType type, @Query( "expirationDate" ) DateWrapper expiration,
+    Call<CouponDTO> create( @Query( "type" ) String type, @Query( "expirationDate" ) DateWrapper expiration,
             @Query( "howMany" ) Integer howMany, @Query( "productVersion" ) Integer productVersion,
             @Query( "product" ) Integer product, @Query( "licenseOnly" ) Boolean licenseOnly,
             @Query( "number" ) Integer number );
