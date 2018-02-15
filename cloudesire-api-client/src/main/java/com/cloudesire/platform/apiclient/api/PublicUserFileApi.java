63579e24eb17619a5a7f54f1d93381c3e2abc917
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-02-15 17:55:52 +0100

    Add specific download method requesting an image (#52)

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/PublicUserFileApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/PublicUserFileApi.java
@@ -6,6 +6,7 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
@@ -20,9 +21,13 @@ public interface PublicUserFileApi
     @GET( "publicUserFile/{id}" )
     Call<FileDTO> get( @Path( "id" ) int id );
 
-    @GET( "publicUserFile/static/{name}")
+    @GET( "publicUserFile/static/{name}" )
     Call<ResponseBody> download( @Path( "name" ) String name );
 
-    @DELETE( "publicUserFile/{id}")
+    @GET( "publicUserFile/static/{name}" )
+    @Headers( "Accept: image/*" )
+    Call<ResponseBody> downloadImage( @Path( "name" ) String name );
+
+    @DELETE( "publicUserFile/{id}" )
     Call<Void> delete( @Path( "id" ) int id );
 }
