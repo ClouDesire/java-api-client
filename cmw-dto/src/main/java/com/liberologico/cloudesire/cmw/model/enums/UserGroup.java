525099876c70d8e17ef40e2afd6ea61379fe25a0
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-03-08 14:18:08 +0100

    Add user groups (#60)

---
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/enums/UserGroup.java
@@ -0,0 +1,14 @@
+package com.liberologico.cloudesire.cmw.model.enums;
+
+/**
+ * User groups
+ *
+ * A generic way of applying labels and permissions to more than one user.
+ */
+public enum UserGroup
+{
+    MANAGEMENT,
+    MARKETING,
+    SALES,
+    TECH
+}
