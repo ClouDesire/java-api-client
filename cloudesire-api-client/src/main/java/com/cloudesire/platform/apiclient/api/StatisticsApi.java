71d66af5359247e5f0cdab0941776d1f065ccbd4
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-04-23 12:49:11 +0200

    Add statistics API (#80)

---
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/StatisticsApi.java
@@ -0,0 +1,27 @@
+package com.cloudesire.platform.apiclient.api;
+
+import com.liberologico.cloudesire.cmw.model.dto.StatisticsDTO;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface StatisticsApi
+{
+    @GET( "statistics" )
+    Call<StatisticsDTO> get();
+
+    @GET( "statistics" )
+    Call<StatisticsDTO> get( @Query( "environment" ) String environment );
+
+    @GET( "statistics/{year}" )
+    Call<StatisticsDTO> get( @Path( "year" ) String year, @Query( "environment" ) String environment );
+
+    @GET( "statistics/{year}/{month}" )
+    Call<StatisticsDTO> get( @Path( "year" ) String year, @Path( "month" ) String month,
+            @Query( "environment" ) String environment );
+
+    @GET( "statistics/{year}/{month}/{day}" )
+    Call<StatisticsDTO> get( @Path( "year" ) String year, @Path( "month" ) String month, @Path( "day" ) String day,
+            @Query( "environment" ) String environment );
+}
