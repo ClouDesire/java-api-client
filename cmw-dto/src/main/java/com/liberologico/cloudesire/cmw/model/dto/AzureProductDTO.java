62a950bb78196a2f55b82869686c2bc90263ce43
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-05-23 11:26:12 +0200

    Add price

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/AzureProductDTO.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/AzureProductDTO.java
@@ -3,8 +3,11 @@ package com.liberologico.cloudesire.cmw.model.dto;
 import com.liberologico.cloudesire.cmw.model.enums.MicrosoftProduct;
 import com.liberologico.cloudesire.cmw.model.enums.ProductType;
 
+import java.math.BigDecimal;
+
 public class AzureProductDTO extends ProductDTO
 {
+    private BigDecimal price;
 
     public AzureProductDTO( String name, String identifier, MicrosoftProduct type )
     {
@@ -16,4 +19,13 @@ public class AzureProductDTO extends ProductDTO
     {
     }
 
+    public BigDecimal getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice( BigDecimal price )
+    {
+        this.price = price;
+    }
 }
