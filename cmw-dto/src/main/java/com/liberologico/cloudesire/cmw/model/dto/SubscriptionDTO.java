654654feefdfe55ad2aefba2282983f9c1d0339d
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-05-31 14:10:43 +0200

    Add swagger doc

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/SubscriptionDTO.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/SubscriptionDTO.java
@@ -1,11 +1,14 @@
 package com.liberologico.cloudesire.cmw.model.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.List;
 
 public class SubscriptionDTO extends BaseSubscriptionDTO
 {
     private List<SubscriptionBillingItemDTO> billingItems;
 
+    @ApiModelProperty( value = "API endpoints for this subscription", readOnly = true )
     private List<SubscriptionApiEndpointDTO> apiEndpoints;
 
     public List<SubscriptionBillingItemDTO> getBillingItems()
