e9e9b22166057309f74c5aac09f0b8f0c1613f70
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-06-08 12:51:22 +0200

    Get and execute tasks from admin API

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/AdminApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/AdminApi.java
@@ -1,13 +1,25 @@
 package com.cloudesire.platform.apiclient.api;
 
 import com.liberologico.cloudesire.cmw.model.dto.PageRequestDTO;
+import com.liberologico.cloudesire.cmw.model.dto.TaskExecutorDTO;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+import java.util.List;
 
 public interface AdminApi
 {
+    @GET( "task" )
+    Call<List<TaskExecutorDTO>> getTasks();
+
+    @PATCH( "task/{className}" )
+    Call<Void> executeTask( @Path ( "className" ) String className );
+
     @POST( "admin/report/generate" )
     Call<Void> generateMissingReports( @Body PageRequestDTO page );
 
