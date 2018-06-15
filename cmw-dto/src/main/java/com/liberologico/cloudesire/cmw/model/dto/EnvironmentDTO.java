05f3789293f38dd1eed8173f8fb7b317affd22c4
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-06-15 17:47:03 +0200

    Minor fixes to environment swagger docs (#120)

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/EnvironmentDTO.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/EnvironmentDTO.java
@@ -1212,17 +1212,18 @@ public class EnvironmentDTO extends DTO
         @ApiModelProperty( "Remove customer data from notifications" )
         private boolean gdpr;
 
-        public boolean isExternalSubscriptionHandling()
-        {
-            return externalSubscriptionHandling != null;
-        }
-
+        @ApiModelProperty( hidden = true )
         public boolean isZuoraEnabled()
         {
             return ExternalSubscriptionHandling.ZUORA.equals( externalSubscriptionHandling );
         }
 
         //region Auto-generated getters and setters
+        public boolean isExternalSubscriptionHandling()
+        {
+            return externalSubscriptionHandling != null;
+        }
+
         public boolean getSyndicated()
         {
             return syndicated;
@@ -2268,6 +2269,7 @@ public class EnvironmentDTO extends DTO
     }
 
     @AssertTrue( message = "Missing CSP API connector endpoint" )
+    @ApiModelProperty( hidden = true )
     public boolean isCspConnectorConfigured()
     {
         if ( features.enabledProductTypes.contains( ProductType.CSP ) )
