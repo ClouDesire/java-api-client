0b9d436a89391dcc773c14a26ca665a6e8edfce2
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-06-15 15:41:32 +0200

    Add PricingPatchDTO swagger docs

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/PricingPatchDTO.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/PricingPatchDTO.java
@@ -1,11 +1,16 @@
 package com.liberologico.cloudesire.cmw.model.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
+@ApiModel("Request a price change for a cloud price")
 public class PricingPatchDTO extends DTO
 {
     @NotNull
+    @ApiModelProperty("The new price")
     private BigDecimal price;
 
     public BigDecimal getPrice()
