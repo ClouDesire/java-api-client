34f96e2597250937df4f4ed03445fa49148096bb
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-06-19 14:02:56 +0200

    Use card dto for invoice payment

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/patch/InvoicePaymentReferenceDTO.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/patch/InvoicePaymentReferenceDTO.java
@@ -1,10 +1,11 @@
 package com.liberologico.cloudesire.cmw.model.patch;
 
+import com.liberologico.cloudesire.cmw.model.dto.DTO;
 import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.Size;
 
-public class InvoicePaymentReferenceDTO
+public class InvoicePaymentReferenceDTO extends DTO
 {
     @ApiModelProperty( "Invoice payment reference" )
     @Size( max = 255 )
