6c710e9d30926e43f9626efe02f03efe791d6b5b
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-07-09 15:49:17 +0200

    Document OrderType

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/enums/OrderType.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/enums/OrderType.java
@@ -1,16 +1,31 @@
 package com.liberologico.cloudesire.cmw.model.enums;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("Which kind of order")
 public enum OrderType
 {
+    @ApiModelProperty( "Buy a product" )
     NORMAL,
+    @ApiModelProperty( "Trial for a product" )
     TRIAL,
+    @ApiModelProperty( "Test mode for vendors" )
     SANDBOX,
+    @ApiModelProperty( "Deprecated" )
     UPGRADE,
+    @ApiModelProperty( "Change values for the available Billing Item" )
     BILLING_ITEM_UPGRADE,
+    @ApiModelProperty( "Renew an active subscription" )
     RENEWAL,
+    @ApiModelProperty( "An order without expiration" )
     UNMANAGED,
+    @ApiModelProperty( "Request termination of an active subscription" )
     TERMINATION,
+    @ApiModelProperty( "For adding unmanaged costs to an active subscription" )
     UPSELL,
+    @ApiModelProperty( "Upgrade disk space, only for managed products" )
     DISK_UPGRADE,
+    @ApiModelProperty( "Plan upgrade, only for syndicated products" )
     SYNDICATED_UPGRADE
 }
