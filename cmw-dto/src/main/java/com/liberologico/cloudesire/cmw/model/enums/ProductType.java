4d0d25be1d83e68ed19c25d7d97ac6c15261af57
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-07-10 16:37:19 +0200

    Support bare VM product type (#130)
    * Add user public key
    * Add VM product type

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/enums/ProductType.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/enums/ProductType.java
@@ -1,37 +1,29 @@
 package com.liberologico.cloudesire.cmw.model.enums;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 @ApiModel( "The product type" )
 public enum ProductType
 {
-    /**
-     * This product is billed per API call
-     */
+    @ApiModelProperty( "This product is billed per API call" )
     API,
 
-    /**
-     * This product is a bundle of other products
-     */
+    @ApiModelProperty( "This product is a bundle of other products" )
     BUNDLE,
 
-    /**
-     * This product is imported from the Azure catalog
-     */
+    @ApiModelProperty( "This product is imported from the Azure catalog" )
     CSP,
 
-    /**
-     * Provisioning is made by us
-     */
+    @ApiModelProperty( "Provisioning is made by us" )
     MANAGED,
 
-    /**
-     * Provisioning is handled externally
-     */
+    @ApiModelProperty( "Provisioning is handled externally" )
     SERVICE,
 
-    /**
-     * Provisioning is handled programmatically
-     */
-    SYNDICATED
+    @ApiModelProperty( "Provisioning is handled programmatically" )
+    SYNDICATED,
+
+    @ApiModelProperty( "This product provisions a bare virtual machine" )
+    VM
 }
