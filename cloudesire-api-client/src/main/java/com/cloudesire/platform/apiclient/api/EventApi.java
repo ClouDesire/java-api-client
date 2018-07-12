ff3d0ab5655879bacf7d1a524f840034fe785ac1
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-07-12 10:59:15 +0200

    Retrieve paged events (#134)

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/EventApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/EventApi.java
@@ -4,13 +4,18 @@ import com.liberologico.cloudesire.cmw.model.dto.EventDTO;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
 
 import java.util.List;
+import java.util.Map;
 
 public interface EventApi
 {
     @GET( "event?type=all" )
     Call<List<EventDTO>> getAll();
+
+    @GET( "event?type=all" )
+    Call<List<EventDTO>> getAll( @QueryMap Map<String, String> pageRequest );
 
     @GET( "event?type=entity" )
     Call<List<EventDTO>> getAll(
