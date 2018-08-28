4e5514a399b5b6e96575daaa498d47a14c8af218
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-08-28 11:59:31 +0200

    Add diskSpace in SingleInstancePricingPatchDTO (#163)

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/SingleInstancePricingPatchDTO.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/model/dto/SingleInstancePricingPatchDTO.java
@@ -10,6 +10,9 @@ public class SingleInstancePricingPatchDTO extends PricingPatchDTO
     @ApiModelProperty( "Unlink the Instance Pricing from any Virtual Machine Configuration" )
     private Boolean deprecated;
 
+    @ApiModelProperty( "Change associated diskSpace" )
+    private Integer diskSpace;
+
     public SingleInstancePricingPatchDTO( @NotNull Boolean deprecated )
     {
         this.deprecated = deprecated;
@@ -32,5 +35,15 @@ public class SingleInstancePricingPatchDTO extends PricingPatchDTO
     public void setDeprecated( Boolean deprecated )
     {
         this.deprecated = deprecated;
+    }
+
+    public Integer getDiskSpace()
+    {
+        return diskSpace;
+    }
+
+    public void setDiskSpace( Integer diskSpace )
+    {
+        this.diskSpace = diskSpace;
     }
 }
