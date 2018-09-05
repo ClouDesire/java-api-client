5cb68ce130b15bc9b2b31d500ab14c49b17b23b7
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-09-05 17:48:21 +0200

    Filter InstanceType by CloudProvider ID

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/InstanceTypeApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/InstanceTypeApi.java
@@ -3,11 +3,12 @@ package com.cloudesire.platform.apiclient.api;
 import com.liberologico.cloudesire.cmw.model.dto.InstanceTypeDTO;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 import java.util.List;
 
 public interface InstanceTypeApi
 {
     @GET( "instanceType" )
-    Call<List<InstanceTypeDTO>> getAll();
+    Call<List<InstanceTypeDTO>> getAll( @Query( "cloudProviderId" ) int cloudProviderId );
 }
