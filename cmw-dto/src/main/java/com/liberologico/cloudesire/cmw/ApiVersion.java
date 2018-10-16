d053a54f2aa6cac50cb1e8a93baa91f7c633468a
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-10-16 16:14:02 +0200

    Add new API version V20181016
    Mark old fields in Coupon as deprecated since this version

--- cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/ApiVersion.java
+++ cmw-dto/src/main/java/com/liberologico/cloudesire/cmw/ApiVersion.java
@@ -2,6 +2,7 @@ package com.liberologico.cloudesire.cmw;
 
 public final class ApiVersion
 {
+    public static final long V20181016 = 20181016;
     public static final long V20180905 = 20180905;
     public static final long V20180629 = 20180629;
     public static final long V20180504 = 20180504;
