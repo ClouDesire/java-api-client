4025d60caf86b526ace1b5acf952debd8e153db1
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-10-31 17:28:00 +0100

    wip

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/EventApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/EventApi.java
@@ -2,6 +2,7 @@ package com.cloudesire.platform.apiclient.api;
 
 import com.liberologico.cloudesire.cmw.model.dto.EventDTO;
 import retrofit2.Call;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
@@ -11,8 +12,17 @@ import java.util.Map;
 
 public interface EventApi
 {
-    @GET( "event?type=all" )
+    @GET( "event" )
     Call<List<EventDTO>> getAll();
+
+    @GET( "event" )
+    Call<List<EventDTO>> getAll( @Query("type") EventType type );
+
+    @GET( "event" )
+    Call<List<EventDTO>> getAll( @Query("type") EventType type, @Query("entity") String entity, @Query("id") int entiyId );
+
+    @GET( "event/annotated")
+    Call<List<String>> getAnnotated();
 
     @GET( "event?type=all" )
     Call<List<EventDTO>> getAll( @QueryMap Map<String, String> pageRequest );
@@ -24,4 +34,20 @@ public interface EventApi
 
     @GET( "event?type=all" )
     Call<List<EventDTO>> getAll( @Query("type") boolean notified );
+
+    @DELETE( "event" )
+    Call<Void> delete();
+
+
+    enum EventType
+    {
+        ALL,
+        ENTITY;
+
+        @Override
+        public String toString()
+        {
+            return name().toLowerCase();
+        }
+    }
 }
