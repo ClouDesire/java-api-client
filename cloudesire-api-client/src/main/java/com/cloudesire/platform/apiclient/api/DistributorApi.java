4d218705bb35f37d200db71a4ffc49c8f4f1a258
Matteo Giordano <matteo.giordano@cloudesire.com>
2018-12-13 11:09:52 +0100

    Add companies enabled status

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/DistributorApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/DistributorApi.java
@@ -1,10 +1,12 @@
 package com.cloudesire.platform.apiclient.api;
 
 import com.liberologico.cloudesire.cmw.model.dto.DistributorDTO;
+import com.liberologico.cloudesire.cmw.model.patch.BaseCompanyPatchDTO;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
@@ -29,6 +31,9 @@ public interface DistributorApi
 
     @PUT( "distributor/{id}" )
     Call<DistributorDTO> update( @Path( "id" ) int id, @Body DistributorDTO distributor );
+
+    @PATCH( "distributor/{id}" )
+    Call<Void> partialUpdate( @Path( "id" ) int id, @Body BaseCompanyPatchDTO patch );
 
     @DELETE( "distributor/{id}" )
     Call<Void> delete( @Path( "id" ) int id );
--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/ResellerApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/ResellerApi.java
@@ -1,10 +1,12 @@
 package com.cloudesire.platform.apiclient.api;
 
 import com.liberologico.cloudesire.cmw.model.dto.ResellerDTO;
+import com.liberologico.cloudesire.cmw.model.patch.BaseCompanyPatchDTO;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
@@ -35,6 +37,9 @@ public interface ResellerApi
 
     @PUT( "reseller/{id}" )
     Call<ResellerDTO> update( @Path( "id" ) int id, @Body ResellerDTO reseller );
+
+    @PATCH( "reseller/{id}" )
+    Call<Void> partialUpdate( @Path( "id" ) int id, @Body BaseCompanyPatchDTO patch );
 
     @DELETE( "reseller/{id}" )
     Call<Void> delete( @Path( "id" ) int id );
