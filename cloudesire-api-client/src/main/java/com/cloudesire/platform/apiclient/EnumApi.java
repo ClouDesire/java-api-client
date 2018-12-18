48675c09abc61aa8e87b45568b544fe8b11d6c05
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2018-12-18 16:08:29 +0100

    cleanup

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/EnumApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/EnumApi.java
@@ -2,11 +2,12 @@ package com.cloudesire.platform.apiclient;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 import java.util.List;
 
 public interface EnumApi
 {
     @GET( "enum/{name}" )
-    Call<List<String>> get( String name );
+    Call<List<String>> get( @Path( "name" ) String name );
 }
