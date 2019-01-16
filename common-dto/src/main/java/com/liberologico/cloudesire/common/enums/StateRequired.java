e35e7f50fde4f6605628704a687aea85417ef5bc
Matteo Giordano <matteo.giordano@cloudesire.com>
2019-01-16 12:59:59 +0100

    Add StateRequired enum

---
+++ common-dto/src/main/java/com/liberologico/cloudesire/common/enums/StateRequired.java
@@ -0,0 +1,9 @@
+package com.liberologico.cloudesire.common.enums;
+
+/**
+ * Two-letter country codes for which a "state" is required for a valid address
+ */
+public enum StateRequired
+{
+    US, CA, AU, CN, MX, MY, IT
+}
