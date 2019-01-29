1f331a31c729b689a73899596b01ac13e7a76fad
Matteo Giordano <matteo.giordano@cloudesire.com>
2019-01-29 18:32:11 +0100

    Filter invoices by date

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/query/InvoiceQuery.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/query/InvoiceQuery.java
@@ -1,18 +1,29 @@
 package com.cloudesire.platform.apiclient.query;
 
 import com.cloudesire.platform.apiclient.dto.model.enums.OrderType;
+import com.liberologico.cloudesire.common.SimpleDateFormatFactory;
+
+import java.util.Date;
 
 public class InvoiceQuery extends BaseQuery
 {
     private static final String EXPIRED = "expired";
+    private static final String FROM = "from";
     private static final String NOMINEE = "nominee";
     private static final String PAID = "paid";
     private static final String SELF_BILLED = "selfBilled";
+    private static final String TO = "to";
     private static final String TYPE = "type";
 
     public InvoiceQuery setExpired( boolean expired )
     {
         put( EXPIRED, expired );
+        return this;
+    }
+
+    public InvoiceQuery setFrom( Date from )
+    {
+        put( FROM, SimpleDateFormatFactory.dateFormat().format( from ) );
         return this;
     }
 
@@ -37,6 +48,12 @@ public class InvoiceQuery extends BaseQuery
     public InvoiceQuery setSelfBilled( Boolean selfBilled )
     {
         put( SELF_BILLED, selfBilled );
+        return this;
+    }
+
+    public InvoiceQuery setTo( Date to )
+    {
+        put( TO, SimpleDateFormatFactory.dateFormat().format( to ) );
         return this;
     }
 
