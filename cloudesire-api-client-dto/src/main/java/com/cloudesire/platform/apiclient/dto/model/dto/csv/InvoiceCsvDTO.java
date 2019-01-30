c82ce448ac98001fcebfcccbf45355204d15a754
Matteo Giordano <matteo.giordano@cloudesire.com>
2019-01-30 11:50:19 +0100

    Generate invoice CSV

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/csv/InvoiceCsvDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/csv/InvoiceCsvDTO.java
@@ -1,12 +1,17 @@
 package com.cloudesire.platform.apiclient.dto.model.dto.csv;
 
+import com.cloudesire.platform.apiclient.dto.model.dto.UrlEntityDTO;
+import com.cloudesire.platform.apiclient.dto.model.enums.LineType;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.cloudesire.platform.apiclient.dto.model.dto.DTO;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
-@JsonPropertyOrder( { "remoteId", "nominee", "description", "type", "paymentReceived", "currency", "price", "vat" } )
+@JsonPropertyOrder( {
+        "remoteId", "nominee", "description", "type", "paymentReceived", "currency", "price", "vat",
+        "linePrice", "quantity", "unit", "lineDescription", "lineType", "billingItem", "setup", "discount", "maximum"
+} )
 public class InvoiceCsvDTO extends DTO
 {
     private String remoteId;
@@ -25,8 +30,25 @@ public class InvoiceCsvDTO extends DTO
 
     private BigDecimal vat;
 
-    // region Auto-generated code
+    private BigDecimal linePrice;
 
+    private BigDecimal quantity = BigDecimal.ONE;
+
+    private String unit;
+
+    private String lineDescription;
+
+    private LineType lineType;
+
+    private UrlEntityDTO billingItem;
+
+    private boolean setup;
+
+    private BigDecimal discount;
+
+    private Integer maximum;
+
+    // region Auto-generated code
     public String getRemoteId()
     {
         return remoteId;
@@ -107,5 +129,94 @@ public class InvoiceCsvDTO extends DTO
         this.vat = vat;
     }
 
+    public BigDecimal getLinePrice()
+    {
+        return linePrice;
+    }
+
+    public void setLinePrice( BigDecimal linePrice )
+    {
+        this.linePrice = linePrice;
+    }
+
+    public BigDecimal getQuantity()
+    {
+        return quantity;
+    }
+
+    public void setQuantity( BigDecimal quantity )
+    {
+        this.quantity = quantity;
+    }
+
+    public String getUnit()
+    {
+        return unit;
+    }
+
+    public void setUnit( String unit )
+    {
+        this.unit = unit;
+    }
+
+    public String getLineDescription()
+    {
+        return lineDescription;
+    }
+
+    public void setLineDescription( String lineDescription )
+    {
+        this.lineDescription = lineDescription;
+    }
+
+    public LineType getLineType()
+    {
+        return lineType;
+    }
+
+    public void setLineType( LineType lineType )
+    {
+        this.lineType = lineType;
+    }
+
+    public UrlEntityDTO getBillingItem()
+    {
+        return billingItem;
+    }
+
+    public void setBillingItem( UrlEntityDTO billingItem )
+    {
+        this.billingItem = billingItem;
+    }
+
+    public boolean isSetup()
+    {
+        return setup;
+    }
+
+    public void setSetup( boolean setup )
+    {
+        this.setup = setup;
+    }
+
+    public BigDecimal getDiscount()
+    {
+        return discount;
+    }
+
+    public void setDiscount( BigDecimal discount )
+    {
+        this.discount = discount;
+    }
+
+    public Integer getMaximum()
+    {
+        return maximum;
+    }
+
+    public void setMaximum( Integer maximum )
+    {
+        this.maximum = maximum;
+    }
     // endregion
 }
