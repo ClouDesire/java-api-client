019f59da180866605ca3324e1a5fa5139a338843
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2019-02-15 10:28:52 +0100

    Simplify exception keys

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/constants/ErrorKeys.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/constants/ErrorKeys.java
@@ -5,19 +5,19 @@ package com.cloudesire.platform.apiclient.dto.model.constants;
  */
 public final class ErrorKeys
 {
-    public static final String INVALID_SIZE = "{wrong.size.%element%.%max%.%min%}";
-    public static final String INVALID_MIN = "{must.be.greater.than.or.equal.to.%min%}";
-    public static final String INVALID_MAX = "{must.be.lower.than.or.equal.to.%max%}";
-    public static final String ALPHANUMERIC = "{must.be.alphanumeric.%element%}";
-    public static final String ALPHANUMERIC_START_WITH_LETTER = "{must.be.alphanumeric.and.start.with.letter.%element%}";
-    public static final String TWO_DIGITS = "{must.contain.two.digits.%element%}";
-    public static final String TWO_OR_FOUR_DIGITS = "{must.contain.two.or.four.digits.%element%}";
-    public static final String THREE_OR_FOUR_DIGITS = "{must.contain.three.or.four.digits.%element%}";
-    public static final String ALPHANUMERIC_UNDERSCORE = "{must.be.alphanumeric.underscore.is.permitted.%element%}";
-    public static final String CHARACTERS_NOT_PERMITTED = "{characters.not.permitted.%element%}";
-    public static final String CHARACTERS_NOT_PERMITTED_RESELLER = "{characters.not.permitted.reseller.%element%}";
-    public static final String COMPANY_NOT_ALLOWED = "{company.not.allowed.%element%}";
-    public static final String FEATURE_DISABLED = "{feature.disabled.%element%}";
+    public static final String INVALID_SIZE = "wrong.size.%max%.%min%";
+    public static final String INVALID_MIN = "must.be.greater.than.or.equal.to.%min%";
+    public static final String INVALID_MAX = "must.be.lower.than.or.equal.to.%max%";
+    public static final String ALPHANUMERIC = "must.be.alphanumeric";
+    public static final String ALPHANUMERIC_START_WITH_LETTER = "must.be.alphanumeric.and.start.with.letter";
+    public static final String TWO_DIGITS = "must.contain.two.digits";
+    public static final String TWO_OR_FOUR_DIGITS = "must.contain.two.or.four.digits";
+    public static final String THREE_OR_FOUR_DIGITS = "must.contain.three.or.four.digits";
+    public static final String ALPHANUMERIC_UNDERSCORE = "must.be.alphanumeric.underscore.is.permitted";
+    public static final String CHARACTERS_NOT_PERMITTED = "characters.not.permitted";
+    public static final String CHARACTERS_NOT_PERMITTED_RESELLER = "characters.not.permitted.reseller";
+    public static final String COMPANY_NOT_ALLOWED = "company.not.allowed";
+    public static final String FEATURE_DISABLED = "feature.disabled";
 
     private ErrorKeys()
     {
