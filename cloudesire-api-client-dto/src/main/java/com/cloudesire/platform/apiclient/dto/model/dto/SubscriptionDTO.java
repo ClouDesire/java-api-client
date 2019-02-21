820122babda923c16cf6b8f967c0a6b3d12e5531
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2019-02-21 14:35:08 +0100

    Frontend wants availableOperations on subscription list

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/SubscriptionDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/SubscriptionDTO.java
@@ -1,13 +1,17 @@
 package com.cloudesire.platform.apiclient.dto.model.dto;
 
+import com.liberologico.cloudesire.common.enums.dto.ActionDTO;
 import io.swagger.annotations.ApiModelProperty;
 
 import java.util.List;
+import java.util.Set;
 
 public class SubscriptionDTO extends BaseSubscriptionDTO
 {
     @ApiModelProperty( value = "Extra resources for this subscription", readOnly = true )
     private List<SubscriptionBillingItemDTO> billingItems;
+
+    private Set<ActionDTO> availableOperations = null;
 
     public List<SubscriptionBillingItemDTO> getBillingItems()
     {
@@ -17,5 +21,15 @@ public class SubscriptionDTO extends BaseSubscriptionDTO
     public void setBillingItems( List<SubscriptionBillingItemDTO> billingItems )
     {
         this.billingItems = billingItems;
+    }
+
+    public Set<ActionDTO> getAvailableOperations()
+    {
+        return availableOperations;
+    }
+
+    public void setAvailableOperations( Set<ActionDTO> availableOperations )
+    {
+        this.availableOperations = availableOperations;
     }
 }
--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/SubscriptionDetailDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/SubscriptionDetailDTO.java
@@ -1,25 +1,11 @@
 package com.cloudesire.platform.apiclient.dto.model.dto;
 
-import com.liberologico.cloudesire.common.enums.dto.ActionDTO;
-
 import java.util.List;
-import java.util.Set;
 
 public class SubscriptionDetailDTO extends SubscriptionDTO
 {
-    private Set<ActionDTO> availableOperations = null;
 
     private List<UrlEntityDTO> possibleUpgrades;
-
-    public Set<ActionDTO> getAvailableOperations()
-    {
-        return availableOperations;
-    }
-
-    public void setAvailableOperations( Set<ActionDTO> availableOperations )
-    {
-        this.availableOperations = availableOperations;
-    }
 
     public List<UrlEntityDTO> getPossibleUpgrades()
     {
