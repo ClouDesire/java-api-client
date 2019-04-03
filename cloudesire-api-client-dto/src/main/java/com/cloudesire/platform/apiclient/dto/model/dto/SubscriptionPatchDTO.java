2df56fb1c23d4e1d031579d9f3506ac6dda05c94
Matteo Giordano <matteo.giordano@cloudesire.com>
2019-04-03 11:28:08 +0200

    Add action for subscription approval (#272)

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/SubscriptionPatchDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/SubscriptionPatchDTO.java
@@ -266,5 +266,8 @@ public class SubscriptionPatchDTO extends DTO
 
         @ApiModelProperty( "Update extra resources ranges with current plan values" )
         REFRESH_BILLING_ITEM_VALUES,
+
+        @ApiModelProperty( "Approve a subscription" )
+        APPROVE
     }
 }
