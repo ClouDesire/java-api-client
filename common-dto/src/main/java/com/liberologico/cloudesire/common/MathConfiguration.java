96f4ee31fc1e845606fdf2c539e256d774bff067
Matteo Giordano <matteo.giordano@cloudesire.com>
2019-04-10 16:51:48 +0200

    Provide day-scale math context

--- common-dto/src/main/java/com/liberologico/cloudesire/common/MathConfiguration.java
+++ common-dto/src/main/java/com/liberologico/cloudesire/common/MathConfiguration.java
@@ -1,15 +1,36 @@
 package com.liberologico.cloudesire.common;
 
+import java.math.MathContext;
 import java.math.RoundingMode;
 
 public class MathConfiguration
 {
-    /** BigDecimal output precision **/
+    /**
+     * BigDecimal output precision
+     */
     public static final int DEFAULT_PRECISION = 2;
-    /** BigDecimal calculation precision **/
+
+    /**
+     * BigDecimal calculation precision
+     */
     public static final int COMPUTATION_PRECISION = 4;
-    /** BigDecimal day scale precision **/
-    public static final int DAY_SCALE_PRECISION = 10;
-    /** BigDecimal rounding mode **/
+
+    /**
+     * BigDecimal day scale precision
+     */
+    private static final int DAY_SCALE_PRECISION = 10;
+
+    /**
+     * BigDecimal rounding mode
+     */
     public static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_UP;
+
+    public static MathContext dayScaleMathContext()
+    {
+        return new MathContext( DAY_SCALE_PRECISION, ROUNDING_MODE );
+    }
+
+    private MathConfiguration()
+    {
+    }
 }
