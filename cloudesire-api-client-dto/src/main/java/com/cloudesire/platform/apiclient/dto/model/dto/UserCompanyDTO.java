f35867480643b3b0a488e2a96b7d297a68686d50
Matteo Giordano <matteo.giordano@cloudesire.com>
2019-07-05 17:38:58 +0200

    Move VAT exemption to UserCompanyDTO

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/UserCompanyDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/UserCompanyDTO.java
@@ -1,10 +1,22 @@
 package com.cloudesire.platform.apiclient.dto.model.dto;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.Date;
 
 @ApiModel( description = "Company data of a business customer" )
 public class UserCompanyDTO extends CompanyDTO
 {
+    @ApiModelProperty( "The reference number of a VAT Exemption document valid under Italian laws" )
+    private String vatExemptionDeclarationNumber;
+
+    @ApiModelProperty( "The declaration date of a VAT Exemption document valid under Italian laws" )
+    private Date vatExemptionDeclarationDate;
+
+    @ApiModelProperty( "The internal protocol number of a VAT Exemption document valid under Italian laws" )
+    private String vatExemptionInternalProtocolNumber;
+
     public UserCompanyDTO( String name, String email )
     {
         super( name );
@@ -19,5 +31,35 @@ public class UserCompanyDTO extends CompanyDTO
     public UserCompanyDTO()
     {
         super();
+    }
+
+    public String getVatExemptionDeclarationNumber()
+    {
+        return vatExemptionDeclarationNumber;
+    }
+
+    public void setVatExemptionDeclarationNumber( String vatExemptionDeclarationNumber )
+    {
+        this.vatExemptionDeclarationNumber = vatExemptionDeclarationNumber;
+    }
+
+    public Date getVatExemptionDeclarationDate()
+    {
+        return vatExemptionDeclarationDate;
+    }
+
+    public void setVatExemptionDeclarationDate( Date vatExemptionDeclarationDate )
+    {
+        this.vatExemptionDeclarationDate = vatExemptionDeclarationDate;
+    }
+
+    public String getVatExemptionInternalProtocolNumber()
+    {
+        return vatExemptionInternalProtocolNumber;
+    }
+
+    public void setVatExemptionInternalProtocolNumber( String vatExemptionInternalProtocolNumber )
+    {
+        this.vatExemptionInternalProtocolNumber = vatExemptionInternalProtocolNumber;
     }
 }
