d4c070830ad7fe5ee3fc4f121a09755dc47e3665
Matteo Giordano <matteo.giordano@cloudesire.com>
2019-07-17 15:25:05 +0200

    Discount IAAS costs per order duration (#347)

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/CloudProviderDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/CloudProviderDTO.java
@@ -4,6 +4,7 @@ import com.liberologico.cloudesire.common.enums.OSType;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -42,6 +43,9 @@ public class CloudProviderDTO extends NamedEntityDTO
 
     @ApiModelProperty( value = "Supported operating systems", readOnly = true )
     private List<OSType> supportedOperatingSystems;
+
+    @ApiModelProperty( "Map a minimum order duration to a discount percentage for IAAS costs" )
+    private Map<Integer, BigDecimal> discounts;
 
     public String getAlias()
     {
@@ -139,6 +143,16 @@ public class CloudProviderDTO extends NamedEntityDTO
     public void setSupportedOperatingSystems( List<OSType> supportedOperatingSystems )
     {
         this.supportedOperatingSystems = supportedOperatingSystems;
+    }
+
+    public Map<Integer, BigDecimal> getDiscounts()
+    {
+        return discounts;
+    }
+
+    public void setDiscounts( Map<Integer, BigDecimal> discounts )
+    {
+        this.discounts = discounts;
     }
 
     @Override
