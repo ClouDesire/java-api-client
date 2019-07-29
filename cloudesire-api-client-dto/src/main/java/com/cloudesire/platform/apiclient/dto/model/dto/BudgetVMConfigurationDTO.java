2f68192898f084d707a5a2a9dc64f76cee3b0acc
Matteo Giordano <matteo.giordano@cloudesire.com>
2019-07-29 11:23:20 +0200

    Update cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/BudgetVMConfigurationDTO.java
    Co-Authored-By: Giovanni Toraldo <giovanni.toraldo@cloudesire.com>

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/BudgetVMConfigurationDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/BudgetVMConfigurationDTO.java
@@ -3,7 +3,7 @@ package com.cloudesire.platform.apiclient.dto.model.dto;
 import com.liberologico.cloudesire.common.enums.OSType;
 import io.swagger.annotations.ApiModel;
 
-@ApiModel( description = "" )
+@ApiModel( description = "Which cloud resources the customer will get for this order" )
 public class BudgetVMConfigurationDTO extends DTO
 {
     private OSType operatingSystem;
