6edc13dec1cae4c28aab44af52cb7f404cda1368
Matteo Giordano <matteo.giordano@cloudesire.com>
2019-08-06 17:58:50 +0200

    Deprecate GET /budgetEstimate (#351)

--- cloudesire-api-client/src/test/java/com/cloudesire/platform/apiclient/test/CloudesireClientTest.java
+++ cloudesire-api-client/src/test/java/com/cloudesire/platform/apiclient/test/CloudesireClientTest.java
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class CloudesireClientTest
 {
-    private static final String USER_AGENT = "Cloudesire API Client 20190730";
+    private static final String USER_AGENT = "Cloudesire API Client 20190806";
 
     private CloudesireClientCallExecutor executor;
 
