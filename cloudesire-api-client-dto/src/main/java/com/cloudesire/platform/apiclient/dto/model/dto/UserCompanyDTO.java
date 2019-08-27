3ccb778e3a5af06fcc51d32c74f6e81b58c7a8f9
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2019-08-27 14:39:31 +0200

    Add delayed payments support

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/UserCompanyDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/UserCompanyDTO.java
@@ -1,11 +1,17 @@
 package com.cloudesire.platform.apiclient.dto.model.dto;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import java.util.Objects;
 
 @ApiModel( description = "Company data of a business customer" )
 public class UserCompanyDTO extends CompanyDTO
 {
     private VatExemptionDTO vatExemption;
+
+    @ApiModelProperty( "If this customer can pay later" )
+    private boolean delayedPayment;
 
     public UserCompanyDTO( String name, String email )
     {
@@ -31,5 +37,31 @@ public class UserCompanyDTO extends CompanyDTO
     public void setVatExemption( VatExemptionDTO vatExemption )
     {
         this.vatExemption = vatExemption;
+    }
+
+    public boolean isDelayedPayment()
+    {
+        return delayedPayment;
+    }
+
+    public void setDelayedPayment( boolean delayedPayment )
+    {
+        this.delayedPayment = delayedPayment;
+    }
+
+    @Override
+    public boolean equals( Object o )
+    {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+        if ( !super.equals( o ) ) return false;
+        UserCompanyDTO that = (UserCompanyDTO) o;
+        return delayedPayment == that.delayedPayment && Objects.equals( vatExemption, that.vatExemption );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( super.hashCode(), vatExemption, delayedPayment );
     }
 }
