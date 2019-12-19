d9edfc6a03b52d25ee9d66dae97ad144308b1602
Matteo Giordano <matteo.giordano@cloudesire.com>
2019-12-19 17:08:21 +0100

    Add weight to product categories

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/ProductCategoryDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/ProductCategoryDTO.java
@@ -3,9 +3,13 @@ package com.cloudesire.platform.apiclient.dto.model.dto;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import java.util.Objects;
+
 @ApiModel( description = "A category of products" )
 public class ProductCategoryDTO extends NamedEntityDTO
 {
+    private Integer weight;
+
     @ApiModelProperty( value = "How many products for the category are present", readOnly = true )
     private Long productCount = 0L;
 
@@ -16,6 +20,16 @@ public class ProductCategoryDTO extends NamedEntityDTO
         return this.entityToken;
     }
 
+    public Integer getWeight()
+    {
+        return weight;
+    }
+
+    public void setWeight( Integer weight )
+    {
+        this.weight = weight;
+    }
+
     public Long getProductCount()
     {
         return productCount;
@@ -24,5 +38,21 @@ public class ProductCategoryDTO extends NamedEntityDTO
     public void setProductCount( Long productCount )
     {
         this.productCount = productCount;
+    }
+
+    @Override
+    public boolean equals( Object o )
+    {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+        if ( ! super.equals( o ) ) return false;
+        ProductCategoryDTO that = (ProductCategoryDTO) o;
+        return Objects.equals( weight, that.weight ) && Objects.equals( productCount, that.productCount );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( super.hashCode(), weight, productCount );
     }
 }
