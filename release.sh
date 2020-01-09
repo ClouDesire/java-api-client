d0126751696f0dc39cae29b70b4ea65cbe1c06a3
Giovanni Toraldo <giovanni.toraldo@eng.it>
2020-01-09 15:40:35 +0100

    Notify dependabot of the new release at the end of release script

--- release.sh
+++ release.sh
@@ -1,6 +1,10 @@
-#!/bin/bash -exu
+#!/bin/bash -eu
 echo Releasing $VERSION and updating $NEXT_VERSION
 ./mvnw versions:set -DnewVersion=${VERSION} scm:checkin -Dmessage="Release $VERSION" -DgenerateBackupPoms=false
 ./mvnw scm:tag
 ./mvnw versions:set -DnewVersion=${NEXT_VERSION}-SNAPSHOT scm:checkin -Dmessage="Preparing for next iteration" -DgenerateBackupPoms=false
 echo Finished!
+
+echo Waiting to notify dependabot of the new release
+sleep 120
+echo curl -H "Authorization: Personal $GH_TOKEN" -d '{"name": "com.cloudesire.platform:cloudesire-api-client", "package-manager": "maven", "version": "'$VERSION'"}' -H "Content-Type:application/json" -X POST https://api.dependabot.com/release_notifications/private
