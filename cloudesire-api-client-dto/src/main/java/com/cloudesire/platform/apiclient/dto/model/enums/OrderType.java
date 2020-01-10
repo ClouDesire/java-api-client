961e22e54742008a792e9caed91f881f8cb6efa6
Matteo Giordano <Matteo.Giordano@eng.it>
2020-01-10 15:17:10 +0100

    Add ESTIMATE budget type

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/OrderType.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/OrderType.java
@@ -29,5 +29,6 @@ public enum OrderType
     @ApiModelProperty( "Upgrade disk space, only for managed products" )
     DISK_UPGRADE,
     @ApiModelProperty( "Plan upgrade, only for syndicated products" )
-    SYNDICATED_UPGRADE
+    SYNDICATED_UPGRADE,
+    ESTIMATE
 }
