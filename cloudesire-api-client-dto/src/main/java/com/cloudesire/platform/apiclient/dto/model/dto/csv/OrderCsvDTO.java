e2ee37f312e52fb042f664b28abae6061eec8d6f
Matteo Giordano <Matteo.Giordano@eng.it>
2020-01-13 12:50:43 +0100

    Revamp order CSV

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/csv/OrderCsvDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/csv/OrderCsvDTO.java
@@ -6,14 +6,33 @@ import com.cloudesire.platform.apiclient.dto.model.dto.DTO;
 import java.math.BigDecimal;
 import java.util.Date;
 
-@JsonPropertyOrder( { "name", "type", "buyer", "date", "endOfPeriod", "currency", "vat" } )
+@JsonPropertyOrder( {
+        "id", "name", "type", "nomineeId", "nominee", "email", "pec", "sdiCode", "address", "fiscalCode", "taxCode",
+        "date", "endOfPeriod", "total", "currency", "vat"
+} )
 public class OrderCsvDTO extends DTO
 {
+    private Integer id;
+
     private String name;
 
     private String type;
 
-    private String buyer;
+    private Integer nomineeId;
+
+    private String nominee;
+
+    private String email;
+
+    private String pec;
+
+    private String sdiCode;
+
+    private String address;
+
+    private String fiscalCode;
+
+    private String taxCode;
 
     private Date date;
 
@@ -23,7 +42,18 @@ public class OrderCsvDTO extends DTO
 
     private BigDecimal vat;
 
+    private BigDecimal total;
+
     // region Auto-generated code
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId( Integer id )
+    {
+        this.id = id;
+    }
 
     public String getName()
     {
@@ -45,14 +75,84 @@ public class OrderCsvDTO extends DTO
         this.type = type;
     }
 
-    public String getBuyer()
+    public Integer getNomineeId()
     {
-        return buyer;
+        return nomineeId;
     }
 
-    public void setBuyer( String buyer )
+    public void setNomineeId( Integer nomineeId )
     {
-        this.buyer = buyer;
+        this.nomineeId = nomineeId;
+    }
+
+    public String getNominee()
+    {
+        return nominee;
+    }
+
+    public void setNominee( String nominee )
+    {
+        this.nominee = nominee;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail( String email )
+    {
+        this.email = email;
+    }
+
+    public String getPec()
+    {
+        return pec;
+    }
+
+    public void setPec( String pec )
+    {
+        this.pec = pec;
+    }
+
+    public String getSdiCode()
+    {
+        return sdiCode;
+    }
+
+    public void setSdiCode( String sdiCode )
+    {
+        this.sdiCode = sdiCode;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress( String address )
+    {
+        this.address = address;
+    }
+
+    public String getFiscalCode()
+    {
+        return fiscalCode;
+    }
+
+    public void setFiscalCode( String fiscalCode )
+    {
+        this.fiscalCode = fiscalCode;
+    }
+
+    public String getTaxCode()
+    {
+        return taxCode;
+    }
+
+    public void setTaxCode( String taxCode )
+    {
+        this.taxCode = taxCode;
     }
 
     public Date getDate()
@@ -95,5 +195,14 @@ public class OrderCsvDTO extends DTO
         this.vat = vat;
     }
 
+    public BigDecimal getTotal()
+    {
+        return total;
+    }
+
+    public void setTotal( BigDecimal total )
+    {
+        this.total = total;
+    }
     // endregion
 }
