6167708bb8164e5f5d7fd5a2e2ca34a2cf5f212d
Matteo Giordano <Matteo.Giordano@eng.it>
2020-01-14 11:38:56 +0100

    Add label to payment data

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/PaymentDataDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/PaymentDataDTO.java
@@ -1,5 +1,19 @@
 package com.cloudesire.platform.apiclient.dto.model.dto;
 
+import javax.validation.constraints.NotEmpty;
+
 public abstract class PaymentDataDTO extends DTO
 {
+    @NotEmpty
+    private String label = "Default";
+
+    public String getLabel()
+    {
+        return label;
+    }
+
+    public void setLabel( String label )
+    {
+        this.label = label;
+    }
 }
--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/PaymentMethodDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/PaymentMethodDTO.java
@@ -1,7 +1,8 @@
 package com.cloudesire.platform.apiclient.dto.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotEmpty;
 
 public class PaymentMethodDTO extends PaymentDataDTO
 {
