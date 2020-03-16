fe94032c5e76b208e8c995286aaf8856e198c80d
Matteo Giordano <Matteo.Giordano@eng.it>
2020-03-16 12:56:20 +0100

    Define costs for custom lines

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/CustomOrderLineDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/CustomOrderLineDTO.java
@@ -1,9 +1,15 @@
 package com.cloudesire.platform.apiclient.dto.model.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import java.math.BigDecimal;
+import java.util.Objects;
 
 abstract class CustomOrderLineDTO extends OrderLineDTO
 {
+    @ApiModelProperty( "Cost for the platform owner" )
+    private BigDecimal cost;
+
     public CustomOrderLineDTO( BigDecimal price )
     {
         this( price, null );
@@ -16,5 +22,31 @@ abstract class CustomOrderLineDTO extends OrderLineDTO
 
     public CustomOrderLineDTO()
     {
+    }
+
+    public BigDecimal getCost()
+    {
+        return cost;
+    }
+
+    public void setCost( BigDecimal cost )
+    {
+        this.cost = cost;
+    }
+
+    @Override
+    public boolean equals( Object o )
+    {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+        if ( ! super.equals( o ) ) return false;
+        CustomOrderLineDTO that = (CustomOrderLineDTO) o;
+        return Objects.equals( cost, that.cost );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( super.hashCode(), cost );
     }
 }
