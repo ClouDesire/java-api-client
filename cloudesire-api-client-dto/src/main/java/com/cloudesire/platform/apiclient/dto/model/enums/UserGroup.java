ec06a8a7f5416e211e47a550f794b7358fce89a5
Matteo Giordano <Matteo.Giordano@eng.it>
2020-03-23 15:38:32 +0100

    Add SUPERMANAGER UserGroup

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/UserGroup.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/UserGroup.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiModel;
 @ApiModel( description = "A generic way of applying labels and permissions to more than one user." )
 public enum UserGroup
 {
+    SUPERMANAGER,
     MANAGEMENT,
     MARKETING,
     SALES,
