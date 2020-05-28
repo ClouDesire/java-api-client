d9b5dc06ccff585b71898d2b88904139319705f6
Matteo Giordano <Matteo.Giordano@eng.it>
2020-05-28 12:02:43 +0200

    Delete reports for an invoice

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/AdminApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/AdminApi.java
@@ -10,6 +10,7 @@ import retrofit2.http.GET;
 import retrofit2.http.PATCH;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
 
 import java.util.List;
@@ -30,6 +31,9 @@ public interface AdminApi
 
     @DELETE( "admin/report" )
     Call<Void> purgeReports();
+
+    @DELETE( "admin/report" )
+    Call<Void> deleteReports( @Query( "invoiceId" ) int invoiceId );
 
     @DELETE( "admin/zip" )
     Call<Void> reloadZipFileConfiguration();
