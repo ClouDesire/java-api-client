dce44e3c4bd4d8fa922132fcfd3dc8df2381aa34
Matteo Giordano <Matteo.Giordano@eng.it>
2020-07-01 14:18:02 +0200

    Query sitemap by reseller or catalog

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/EnvironmentApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/EnvironmentApi.java
@@ -1,6 +1,7 @@
 package com.cloudesire.platform.apiclient.api;
 
 import com.cloudesire.platform.apiclient.dto.model.dto.EnvironmentDTO;
+import com.cloudesire.platform.apiclient.query.ResellerCatalogQuery;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -8,6 +9,7 @@ import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
+import retrofit2.http.QueryMap;
 
 import java.util.List;
 
@@ -24,6 +26,9 @@ public interface EnvironmentApi
 
     @GET( "environment/{name}/sitemap" )
     Call<ResponseBody> getSitemap( @Path( "name" ) String name );
+
+    @GET( "environment/{name}/sitemap" )
+    Call<ResponseBody> getSitemap( @Path( "name" ) String name, @QueryMap ResellerCatalogQuery query );
 
     @DELETE( "environment" )
     Call<Void> reset();
