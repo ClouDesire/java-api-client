7e1203ffd5cb882e72fc8230d3637b4726eae9ca
Matteo Giordano <Matteo.Giordano@eng.it>
2020-07-02 14:31:21 +0200

    Filter channel pricings by product version (#534)

---
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/query/DistributorPricingQuery.java
@@ -0,0 +1,39 @@
+package com.cloudesire.platform.apiclient.query;
+
+public class DistributorPricingQuery extends BaseQuery
+{
+    private static final String CATALOG_ID = "catalogId";
+    private static final String PRODUCT_ID = "productId";
+    private static final String PRODUCT_VERSION_ID = "productVersionId";
+    private static final String DEPRECATED = "deprecated";
+
+    public DistributorPricingQuery setPageRequest( PageRequestQuery pageRequestQuery )
+    {
+        putAll( pageRequestQuery );
+        return this;
+    }
+
+    public DistributorPricingQuery setCatalogId( Integer id )
+    {
+        put( CATALOG_ID, id );
+        return this;
+    }
+
+    public DistributorPricingQuery setProductId( Integer id )
+    {
+        put( PRODUCT_ID, id );
+        return this;
+    }
+
+    public DistributorPricingQuery setProductVersionId( Integer id )
+    {
+        put( PRODUCT_VERSION_ID, id );
+        return this;
+    }
+
+    public DistributorPricingQuery setDeprecated( Boolean deprecated )
+    {
+        put( DEPRECATED, deprecated );
+        return this;
+    }
+}
