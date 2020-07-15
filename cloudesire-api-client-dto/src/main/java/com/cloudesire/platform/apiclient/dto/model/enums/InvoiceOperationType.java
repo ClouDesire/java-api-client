b54de989c5899b9834b5c1e6900d241338bd5543
Matteo Giordano <Matteo.Giordano@eng.it>
2020-07-15 11:20:46 +0200

    Reissue first invoice API (#543)

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/InvoiceOperationType.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/InvoiceOperationType.java
@@ -1,17 +1,21 @@
 package com.cloudesire.platform.apiclient.dto.model.enums;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public enum InvoiceOperationType
 {
-    PAID, // Set the Invoice paid
-    PAY, // Customer can then pay the invoice!
+    @ApiModelProperty( "Set the invoice to paid" )
+    PAID,
 
-    /**
-     * Download invoice PDF
-     */
+    @ApiModelProperty( "Pay the invoice" )
+    PAY,
+
+    @ApiModelProperty( "Download invoice PDF" )
     PDF,
 
-    /**
-     * (re)generate invoice PDF
-     */
-    MAKE_PDF
+    @ApiModelProperty( "(re)generate invoice PDF" )
+    MAKE_PDF,
+
+    @ApiModelProperty( "Reissue the invoice" )
+    REISSUE
 }
