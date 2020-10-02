544b2d068d3b4a75bf0b7378b46e74b8d7357b4f
Matteo Giordano <Matteo.Giordano@eng.it>
2020-10-02 17:28:32 +0200

    Choose counter metric function

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/GaugeMetricFunction.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/GaugeMetricFunction.java
@@ -6,21 +6,34 @@ import io.swagger.annotations.ApiModelProperty;
 @ApiModel( description = "Function to apply for gauge metrics value retrieval" )
 public enum GaugeMetricFunction
 {
-    @ApiModelProperty( "Average value over time" )
-    AVERAGE( "avg_over_time" ),
+    @ApiModelProperty( "Average value over time (gauge)" )
+    AVERAGE( "sum(avg_over_time(%s[%ds]))", false ),
 
-    @ApiModelProperty( "Peak value over time" )
-    PEAK( "max_over_time" );
+    @ApiModelProperty( "Increase value over time (counter)" )
+    INCREASE( "sum(increase(%s[%ds]))", true ),
 
-    private String function;
+    @ApiModelProperty( "Peak value over time (gauge)" )
+    PEAK( "sum(max_over_time(%s[%ds]))", false ),
 
-    GaugeMetricFunction( String function )
+    @ApiModelProperty( "Value over time minus previous (counter)" )
+    VALUE( "sum(%1$s) - sum(min_over_time(%1$s[%2$ds]))", true );
+
+    private final String function;
+    private final boolean counter;
+
+    GaugeMetricFunction( String function, boolean counter )
     {
         this.function = function;
+        this.counter = counter;
     }
 
     public String getFunction()
     {
         return function;
+    }
+
+    public boolean isCounter()
+    {
+        return counter;
     }
 }
