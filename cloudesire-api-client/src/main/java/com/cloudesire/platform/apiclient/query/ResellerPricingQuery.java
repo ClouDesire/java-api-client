d41c39da4b28df4c89ddcd4f86a2bbe30246345d
Matteo Giordano <Matteo.Giordano@eng.it>
2020-10-28 16:31:07 +0100

    Show/hide inherited pricings

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/query/ResellerPricingQuery.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/query/ResellerPricingQuery.java
@@ -1,5 +1,7 @@
 package com.cloudesire.platform.apiclient.query;
 
+import com.cloudesire.platform.apiclient.dto.model.enums.ResellerPricingHierarchy;
+
 public class ResellerPricingQuery extends BaseQuery
 {
     private static final String RESELLER_ID = "resellerId";
@@ -7,6 +9,7 @@ public class ResellerPricingQuery extends BaseQuery
     private static final String PRODUCT_ID = "productId";
     private static final String PRODUCT_VERSION_ID = "productVersionId";
     private static final String UNPRICED = "unpriced";
+    private static final String HIERARCHY = "hierarchy";
 
     public ResellerPricingQuery setPageRequest( PageRequestQuery pageRequestQuery )
     {
@@ -41,6 +44,12 @@ public class ResellerPricingQuery extends BaseQuery
     public ResellerPricingQuery setUnpriced( Boolean unpriced )
     {
         put( UNPRICED, unpriced );
+        return this;
+    }
+
+    public ResellerPricingQuery setHierarchy( ResellerPricingHierarchy hierarchy )
+    {
+        put( HIERARCHY, hierarchy );
         return this;
     }
 }
