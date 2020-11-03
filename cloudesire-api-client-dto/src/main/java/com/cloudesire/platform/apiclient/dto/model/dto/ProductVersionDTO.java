0a3be7182024a096ba5fba5b2502530b442f4fc6
Matteo Giordano <Matteo.Giordano@eng.it>
2020-11-03 12:24:30 +0100

    Revert "Add product version CTA label"

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/ProductVersionDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/ProductVersionDTO.java
@@ -155,9 +155,6 @@ public class ProductVersionDTO extends NamedEntityDTO implements ProductVersionL
     @ApiModelProperty( "Whether a Subscription of the Product Version can be terminated before the natural expiration by its customer" )
     private Boolean unkillable;
 
-    @ApiModelProperty( value = "Marketplace button label customization for the Product Version", example = "\"Buy now!\", \"Request a quote\"" )
-    private String ctaLabel;
-
     public ProductVersionDTO( String name, UrlEntityDTO product )
     {
         super( name );
@@ -620,16 +617,6 @@ public class ProductVersionDTO extends NamedEntityDTO implements ProductVersionL
     public void setUnkillable( Boolean unkillable )
     {
         this.unkillable = unkillable;
-    }
-
-    public String getCtaLabel()
-    {
-        return ctaLabel;
-    }
-
-    public void setCtaLabel( String ctaLabel )
-    {
-        this.ctaLabel = ctaLabel;
     }
 
     @Override
