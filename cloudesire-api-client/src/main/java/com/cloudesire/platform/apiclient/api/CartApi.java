cb0b1fb3169935b71c387a57b5aad43d07669eef
Matteo Giordano <Matteo.Giordano@eng.it>
2020-12-04 15:05:47 +0100

    Create resold

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CartApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CartApi.java
@@ -1,6 +1,7 @@
 package com.cloudesire.platform.apiclient.api;
 
 import com.cloudesire.platform.apiclient.dto.model.dto.CartDTO;
+import com.cloudesire.platform.apiclient.query.ResellerCatalogQuery;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
@@ -8,6 +9,7 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
+import retrofit2.http.QueryMap;
 
 import java.util.List;
 
@@ -15,6 +17,9 @@ public interface CartApi
 {
     @POST( "cart" )
     Call<CartDTO> create( @Body CartDTO cart );
+
+    @POST( "cart" )
+    Call<CartDTO> create( @Body CartDTO cart, @QueryMap ResellerCatalogQuery query );
 
     @GET( "cart" )
     Call<List<CartDTO>> getAll();
