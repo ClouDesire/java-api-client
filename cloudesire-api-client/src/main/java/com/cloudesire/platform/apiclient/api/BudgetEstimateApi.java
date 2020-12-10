e00a330b5ccb46f5b127520ad0188f8df06d1c02
Matteo Giordano <Matteo.Giordano@eng.it>
2020-12-10 16:36:23 +0100

    Budget for cart

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/BudgetEstimateApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/BudgetEstimateApi.java
@@ -8,6 +8,8 @@ import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
+import java.util.List;
+
 import static com.cloudesire.platform.apiclient.response.Headers.MODE;
 
 public interface BudgetEstimateApi
@@ -17,6 +19,9 @@ public interface BudgetEstimateApi
 
     @POST( "budgetEstimate" )
     Call<BudgetDTO> request( @Body BudgetInputDTO input, @Header( MODE ) String environment );
+
+    @POST( "budgetEstimate/cart/{id}" )
+    Call<List<BudgetDTO>> requestForCart( @Path( "id" ) int cartId );
 
     @POST( "budgetEstimate/cart/item/{id}" )
     Call<BudgetDTO> requestForCartItem( @Path( "id" ) int cartItemId );
