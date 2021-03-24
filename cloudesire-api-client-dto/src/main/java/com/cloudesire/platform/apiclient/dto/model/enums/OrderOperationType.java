08335eb153b6d6c30a58c840624b6d1ae50e5c36
Matteo Giordano <Matteo.Giordano@eng.it>
2021-03-24 10:22:46 +0100

    Handle operations

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/OrderOperationType.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/OrderOperationType.java
@@ -1,6 +1,14 @@
 package com.cloudesire.platform.apiclient.dto.model.enums;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public enum OrderOperationType
 {
-    APPROVE, DELETE
+    APPROVE, REJECT, DELETE;
+
+    public static Set<OrderOperationType> approvalOperations()
+    {
+        return EnumSet.of( APPROVE, REJECT );
+    }
 }
