d07828e32bd9d97b5e8dcf49e437b30f9b7041fe
Matteo Giordano <Matteo.Giordano@eng.it>
2021-03-25 12:52:39 +0100

    Add excluded versions to reseller cart

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/ResellerPricingVisibility.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/ResellerPricingVisibility.java
@@ -2,6 +2,9 @@ package com.cloudesire.platform.apiclient.dto.model.enums;
 
 import io.swagger.annotations.ApiModelProperty;
 
+import java.util.EnumSet;
+import java.util.Set;
+
 public enum ResellerPricingVisibility
 {
     @ApiModelProperty( "Product Version is not visible to customer, but is buyable through a reseller" )
@@ -11,5 +14,12 @@ public enum ResellerPricingVisibility
     INCLUDED,
 
     @ApiModelProperty( "Product Version is available for buy" )
-    PRICED
+    PRICED;
+
+    public static Set<ResellerPricingVisibility> buyable( UserRole role )
+    {
+        Set<ResellerPricingVisibility> visibility = EnumSet.of( ResellerPricingVisibility.PRICED );
+        if ( role == UserRole.ROLE_RESELLER ) visibility.add( ResellerPricingVisibility.EXCLUDED );
+        return visibility;
+    }
 }
