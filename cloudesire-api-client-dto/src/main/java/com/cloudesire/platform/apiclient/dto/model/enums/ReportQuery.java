adcd6e1c4c0b96481fcd279ac24cfdd7e84431ae
Matteo Giordano <Matteo.Giordano@eng.it>
2021-05-21 15:47:08 +0200

    Query report lines for FINAL/LIVE lines (#672)

---
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/ReportQuery.java
@@ -0,0 +1,15 @@
+package com.cloudesire.platform.apiclient.dto.model.enums;
+
+import io.swagger.annotations.ApiModelProperty;
+
+public enum ReportQuery
+{
+    @ApiModelProperty( "Query for finalized (in invoice) report lines only (default)" )
+    FINAL,
+
+    @ApiModelProperty( "Query for live pay-per-use report lines only" )
+    LIVE,
+
+    @ApiModelProperty( "Query for finalized report lines and aggregate pay-per-use costs in selected timespan" )
+    AGGREGATE
+}
