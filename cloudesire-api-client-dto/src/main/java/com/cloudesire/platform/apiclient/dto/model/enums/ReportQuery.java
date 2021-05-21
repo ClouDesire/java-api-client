b91c02f903fa6c63fd1fa25ecb73fbd1be6811db
Matteo Giordano <Matteo.Giordano@eng.it>
2021-05-21 12:16:31 +0200

    Document

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/ReportQuery.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/ReportQuery.java
@@ -1,6 +1,15 @@
 package com.cloudesire.platform.apiclient.dto.model.enums;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public enum ReportQuery
 {
-    FINAL, LIVE, AGGREGATE
+    @ApiModelProperty( "Query for finalized (in invoice) report lines only (default)" )
+    FINAL,
+
+    @ApiModelProperty( "Query for live pay-per-use report lines only" )
+    LIVE,
+
+    @ApiModelProperty( "Query for finalized lines and aggregated pay-per-use costs" )
+    AGGREGATE
 }
