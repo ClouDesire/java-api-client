a7004b574fccf94abe0ccb1274d22e48eaac93d2
Matteo Giordano <Matteo.Giordano@eng.it>
2021-06-16 12:08:04 +0200

    Limit subscription per product per single user

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/SubscriptionsPerProduct.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/SubscriptionsPerProduct.java
@@ -4,15 +4,30 @@ import io.swagger.annotations.ApiModelProperty;
 
 public enum SubscriptionsPerProduct
 {
+    @ApiModelProperty( "One subscription per product per user, regardless of company, at a time is allowed" )
+    ONE_PER_USER( true ),
+
     @ApiModelProperty( "One subscription per product for a whole company at a time is allowed" )
-    ONE,
+    ONE_PER_COMPANY( true ),
 
     @ApiModelProperty( "One subscription per product for a company group at a time is allowed" )
-    ONE_PER_COMPANY_GROUP,
+    ONE_PER_COMPANY_GROUP( true ),
 
     @ApiModelProperty( "Only one trial per product" )
-    TRIAL,
+    TRIAL( false ),
 
     @ApiModelProperty( "No check is done" )
-    UNLIMITED
+    UNLIMITED( false );
+
+    private final boolean limited;
+
+    SubscriptionsPerProduct( boolean limited )
+    {
+        this.limited = limited;
+    }
+
+    public boolean isLimited()
+    {
+        return limited;
+    }
 }
