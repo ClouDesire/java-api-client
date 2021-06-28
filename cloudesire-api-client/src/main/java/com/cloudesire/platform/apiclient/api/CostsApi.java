8b29f99cff97f0690fbb1a3c28f35574e7cc6776
Matteo Giordano <Matteo.Giordano@eng.it>
2021-06-28 10:39:05 +0200

    Add query filters

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CostsApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CostsApi.java
@@ -1,8 +1,10 @@
 package com.cloudesire.platform.apiclient.api;
 
 import com.cloudesire.platform.apiclient.dto.model.dto.CostDTO;
+import com.cloudesire.platform.apiclient.query.CostsQuery;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.QueryMap;
 
 import java.util.List;
 
@@ -10,4 +12,7 @@ public interface CostsApi
 {
     @GET( "costs" )
     Call<List<CostDTO>> getAll();
+
+    @GET( "costs" )
+    Call<List<CostDTO>> getAll( @QueryMap CostsQuery query );
 }
---
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/query/CostsQuery.java
@@ -0,0 +1,31 @@
+package com.cloudesire.platform.apiclient.query;
+
+import com.liberologico.cloudesire.common.SimpleDateFormatFactory;
+
+import java.util.Date;
+
+public class CostsQuery extends BaseQuery
+{
+    private static final String FROM = "from";
+    private static final String TO = "to";
+    private static final String SUBSCRIPTION_ID = "subscriptionId";
+
+    public CostsQuery from( Date from )
+    {
+        put( FROM, SimpleDateFormatFactory.iso8601Format().format( from ) );
+        return this;
+    }
+
+    public CostsQuery to( Date to )
+    {
+        put( TO, SimpleDateFormatFactory.iso8601Format().format( to ) );
+        return this;
+    }
+
+    public CostsQuery subscriptionId( int subscriptionId )
+    {
+        put( SUBSCRIPTION_ID, subscriptionId );
+        return this;
+    }
+
+}
