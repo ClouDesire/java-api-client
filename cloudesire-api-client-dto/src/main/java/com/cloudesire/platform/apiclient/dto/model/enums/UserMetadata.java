388fa7216d667d93169352f3db279278ad1a9eeb
Matteo Giordano <Matteo.Giordano@eng.it>
2021-07-01 09:59:33 +0200

    Add UX toggles to UserMetadata enum

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/UserMetadata.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/UserMetadata.java
@@ -3,7 +3,8 @@ package com.cloudesire.platform.apiclient.dto.model.enums;
 public enum UserMetadata
 {
     GEOCALL_ID( "Geocall_ID" ),
-    GEOCALL_REQUESTER_ID( "Geocall_requester_ID" );
+    GEOCALL_REQUESTER_ID( "Geocall_requester_ID" ),
+    UX_INVOICE_TOGGLE( "ux-invoice-toggle" );
 
     private final String key;
 
