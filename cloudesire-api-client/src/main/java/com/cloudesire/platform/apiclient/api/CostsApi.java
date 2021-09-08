8326d1ec765209565abe689c94981169c68ef54b
Matteo Giordano <Matteo.Giordano@eng.it>
2021-09-08 16:14:30 +0200

    Get custom cost description

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CostsApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CostsApi.java
@@ -4,6 +4,7 @@ import com.cloudesire.platform.apiclient.dto.model.dto.CostDTO;
 import com.cloudesire.platform.apiclient.query.CostsQuery;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.QueryMap;
 
 import java.util.List;
@@ -15,4 +16,7 @@ public interface CostsApi
 
     @GET( "costs" )
     Call<List<CostDTO>> getAll( @QueryMap CostsQuery query );
+
+    @GET( "costs/description/custom/{identifier}" )
+    Call<String> getCustomCostDescription( @Path( "identifier" ) String identifier );
 }
