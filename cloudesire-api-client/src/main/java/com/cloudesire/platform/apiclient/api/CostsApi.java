a635ae3ecfdbf0a7910cdc503eeb4b286237e435
Matteo Giordano <Matteo.Giordano@eng.it>
2021-09-13 16:59:22 +0200

    Query cost description identifier as url parameter (#737)

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CostsApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CostsApi.java
@@ -5,7 +5,7 @@ import com.cloudesire.platform.apiclient.dto.model.dto.CostDescriptionDTO;
 import com.cloudesire.platform.apiclient.query.CostsQuery;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
+import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
 
 import java.util.List;
@@ -18,6 +18,6 @@ public interface CostsApi
     @GET( "costs" )
     Call<List<CostDTO>> getAll( @QueryMap CostsQuery query );
 
-    @GET( "costs/description/custom/{identifier}" )
-    Call<CostDescriptionDTO> getCustomCostDescription( @Path( "identifier" ) String identifier );
+    @GET( "costs/description/custom" )
+    Call<CostDescriptionDTO> getCustomCostDescription( @Query( "identifier" ) String identifier );
 }
