00d03cdd051af2e0ea3b4098febefe1758aefb9f
Matteo Giordano <Matteo.Giordano@eng.it>
2021-09-15 16:57:38 +0200

    Use a list of objects

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/ConfigurationParameterApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/ConfigurationParameterApi.java
@@ -1,12 +1,14 @@
 package com.cloudesire.platform.apiclient.api;
 
 import com.cloudesire.platform.apiclient.dto.model.dto.ConfigurationParameterDTO;
-import com.cloudesire.platform.apiclient.dto.model.dto.ConfigurationParameterValuesDTO;
+import com.cloudesire.platform.apiclient.dto.model.dto.ConfigurationParameterValueDTO;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+
+import java.util.List;
 
 public interface ConfigurationParameterApi
 {
@@ -14,5 +16,5 @@ public interface ConfigurationParameterApi
     Call<ConfigurationParameterDTO> get( @Path( "id" ) int id );
 
     @POST( "configurationParameter/{id}/values" )
-    Call<ConfigurationParameterValuesDTO> getValues( @Path( "id" ) int id, @Query( "productVersionId" ) int productVersionId );
+    Call<List<ConfigurationParameterValueDTO>> getValues( @Path( "id" ) int id, @Query( "productVersionId" ) int productVersionId );
 }
