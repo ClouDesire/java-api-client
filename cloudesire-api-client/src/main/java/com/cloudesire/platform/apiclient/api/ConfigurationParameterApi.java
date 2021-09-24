815b3ec71d86a37ea25454e238e644e8134272ec
Matteo Giordano <Matteo.Giordano@eng.it>
2021-09-24 13:03:58 +0200

    Return an object for external conf param values

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/ConfigurationParameterApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/ConfigurationParameterApi.java
@@ -1,14 +1,12 @@
 package com.cloudesire.platform.apiclient.api;
 
 import com.cloudesire.platform.apiclient.dto.model.dto.ConfigurationParameterDTO;
-import com.cloudesire.platform.apiclient.dto.model.dto.ConfigurationParameterValueDTO;
+import com.cloudesire.platform.apiclient.dto.model.dto.ConfigurationParameterValuesDTO;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
-
-import java.util.List;
 
 public interface ConfigurationParameterApi
 {
@@ -16,5 +14,5 @@ public interface ConfigurationParameterApi
     Call<ConfigurationParameterDTO> get( @Path( "id" ) int id );
 
     @POST( "configurationParameter/{id}/values" )
-    Call<List<ConfigurationParameterValueDTO>> getValues( @Path( "id" ) int id, @Query( "productVersionId" ) int productVersionId );
+    Call<ConfigurationParameterValuesDTO> getValues( @Path( "id" ) int id, @Query( "productVersionId" ) int productVersionId );
 }
