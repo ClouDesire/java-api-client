0222726858bed944713daa5376d5b13ccea057f4
Matteo Giordano <Matteo.Giordano@eng.it>
2021-10-01 17:07:47 +0200

    Pass current parameters to external values service

---
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/ExternalConfigurationParameterValuesRequestDTO.java
@@ -0,0 +1,35 @@
+package com.cloudesire.platform.apiclient.dto.model.dto;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
+public class ExternalConfigurationParameterValuesRequestDTO implements DTO
+{
+    @NotNull
+    @Valid
+    private UrlEntityDTO productVersion;
+
+    @Valid
+    private Map<UrlEntityDTO, String> configurationParameters;
+
+    public UrlEntityDTO getProductVersion()
+    {
+        return productVersion;
+    }
+
+    public void setProductVersion( UrlEntityDTO productVersion )
+    {
+        this.productVersion = productVersion;
+    }
+
+    public Map<UrlEntityDTO, String> getConfigurationParameters()
+    {
+        return configurationParameters;
+    }
+
+    public void setConfigurationParameters( Map<UrlEntityDTO, String> configurationParameters )
+    {
+        this.configurationParameters = configurationParameters;
+    }
+}
