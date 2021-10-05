c4d581688a9b570ec3b1da7b03c9bed22664edbf
Matteo Giordano <Matteo.Giordano@eng.it>
2021-10-05 10:38:26 +0200

    Document

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/BillingItemPresence.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/BillingItemPresence.java
@@ -1,6 +1,17 @@
 package com.cloudesire.platform.apiclient.dto.model.enums;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel( "Configuration for the presence of the Billing Item when buying a Subscription" )
 public enum BillingItemPresence
 {
-    OPTIONAL, REQUIRED, RECOMMENDED
+    @ApiModelProperty( "The Billing Item is optional" )
+    OPTIONAL,
+
+    @ApiModelProperty( "The Billing Item will be included in every Subscription" )
+    REQUIRED,
+
+    @ApiModelProperty( "The Billing Item will be suggested for inclusion on purchase" )
+    RECOMMENDED
 }
