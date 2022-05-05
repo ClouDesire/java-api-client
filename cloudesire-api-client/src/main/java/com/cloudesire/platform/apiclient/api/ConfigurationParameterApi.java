d2160f1a302c9375103540475403869f619dd1ac
Matteo Giordano <Matteo.Giordano@eng.it>
2022-05-05 10:08:29 +0200

    Retrieve configuration parameters for a product

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/ConfigurationParameterApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/ConfigurationParameterApi.java
@@ -8,9 +8,15 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+import java.util.List;
 
 public interface ConfigurationParameterApi
 {
+    @GET( "configurationParameter" )
+    Call<List<ConfigurationParameterDTO>> getAllForProduct( @Query( "productId" ) int productId );
+
     @GET( "configurationParameter/{id}" )
     Call<ConfigurationParameterDTO> get( @Path( "id" ) int id );
 
