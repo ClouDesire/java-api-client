1659a86517a3447735220aa4f7dbd24d3ca61b88
Matteo Giordano <matteo.giordano@eng.it>
2022-08-30 12:31:11 +0200

    Request customer costs as CSV

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CostsApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CostsApi.java
@@ -3,10 +3,9 @@ package com.cloudesire.platform.apiclient.api;
 import com.cloudesire.platform.apiclient.dto.model.dto.CostDTO;
 import com.cloudesire.platform.apiclient.dto.model.dto.CostDescriptionDTO;
 import com.cloudesire.platform.apiclient.query.CostsQuery;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
-import retrofit2.http.QueryMap;
+import retrofit2.http.*;
 
 import java.util.List;
 
@@ -17,6 +16,11 @@ public interface CostsApi
 
     @GET( "costs" )
     Call<List<CostDTO>> getAll( @QueryMap CostsQuery query );
+
+    @GET( "costs" )
+    @Headers( { "Accept:text/csv" } )
+    @Streaming
+    Call<ResponseBody> getCsv( @QueryMap CostsQuery query );
 
     @GET( "costs/description/custom" )
     Call<CostDescriptionDTO> getCustomCostDescription( @Query( "identifier" ) String identifier );
