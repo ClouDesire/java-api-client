75f689865964b36a68480239f32b1865dda84d05
Matteo Giordano <matteo.giordano@eng.it>
2022-08-31 12:40:56 +0200

    Implement equals

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/BudgetDetailDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/BudgetDetailDTO.java
@@ -5,6 +5,7 @@ import io.swagger.annotations.ApiModelProperty;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 @ApiModel( description = "Budget estimate with line detail" )
 public class BudgetDetailDTO extends BudgetDTO
@@ -33,5 +34,21 @@ public class BudgetDetailDTO extends BudgetDTO
     public void setPostLines( List<OrderLineDTO> postLines )
     {
         this.postLines = postLines;
+    }
+
+    @Override
+    public boolean equals( Object o )
+    {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+        if ( ! super.equals( o ) ) return false;
+        BudgetDetailDTO that = (BudgetDetailDTO) o;
+        return Objects.equals( lines, that.lines );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( super.hashCode(), lines );
     }
 }
