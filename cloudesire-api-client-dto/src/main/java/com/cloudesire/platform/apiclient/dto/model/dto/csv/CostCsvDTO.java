8cc9faf13561c18e93bfa9d5cd0e2f2ed751a12f
Matteo Giordano <Matteo.Giordano@eng.it>
2022-08-31 11:34:14 +0200

    Request customer costs as CSV (#834)

---
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/csv/CostCsvDTO.java
@@ -0,0 +1,73 @@
+package com.cloudesire.platform.apiclient.dto.model.dto.csv;
+
+import com.cloudesire.platform.apiclient.dto.model.dto.DTO;
+import com.cloudesire.platform.apiclient.dto.model.enums.LineType;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import java.math.BigDecimal;
+
+@JsonPropertyOrder( {
+        "subscription", "type", "billingItem", "identifier", "cost"
+} )
+public class CostCsvDTO implements DTO
+{
+    private Integer subscription;
+
+    private LineType type;
+
+    private String billingItem;
+
+    private String identifier;
+
+    private BigDecimal cost;
+
+    public Integer getSubscription()
+    {
+        return subscription;
+    }
+
+    public void setSubscription( Integer subscription )
+    {
+        this.subscription = subscription;
+    }
+
+    public LineType getType()
+    {
+        return type;
+    }
+
+    public void setType( LineType type )
+    {
+        this.type = type;
+    }
+
+    public String getBillingItem()
+    {
+        return billingItem;
+    }
+
+    public void setBillingItem( String billingItem )
+    {
+        this.billingItem = billingItem;
+    }
+
+    public String getIdentifier()
+    {
+        return identifier;
+    }
+
+    public void setIdentifier( String identifier )
+    {
+        this.identifier = identifier;
+    }
+
+    public BigDecimal getCost()
+    {
+        return cost;
+    }
+
+    public void setCost( BigDecimal cost )
+    {
+        this.cost = cost;
+    }
+}
