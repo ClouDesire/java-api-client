6b45a772ae43ad747926825feb954821371b32d3
Matteo Giordano <Matteo.Giordano@eng.it>
2022-12-19 13:40:23 +0100

    Configure billing budget filters

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/BillingBudgetFilterDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/BillingBudgetFilterDTO.java
@@ -2,14 +2,22 @@ package com.cloudesire.platform.apiclient.dto.model.dto;
 
 import com.cloudesire.platform.apiclient.dto.model.enums.CalendarPeriod;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.NotNull;
+import java.util.Map;
 
-@ApiModel( description = "A timeframe definition for a budget." )
+@ApiModel( description = "A timeframe and filters for calculating a budget." )
 public class BillingBudgetFilterDTO implements DTO
 {
     @NotNull
     private CalendarPeriod calendarPeriod = CalendarPeriod.MONTH;
+
+    @ApiModelProperty( "Filter by subscription metadata" )
+    private Map<String, String> metadata;
+
+    @ApiModelProperty( "Filter by cost tags" )
+    private Map<String, String> tags;
 
     public BillingBudgetFilterDTO( CalendarPeriod calendarPeriod )
     {
@@ -28,5 +36,25 @@ public class BillingBudgetFilterDTO implements DTO
     public void setCalendarPeriod( CalendarPeriod calendarPeriod )
     {
         this.calendarPeriod = calendarPeriod;
+    }
+
+    public Map<String, String> getMetadata()
+    {
+        return metadata;
+    }
+
+    public void setMetadata( Map<String, String> metadata )
+    {
+        this.metadata = metadata;
+    }
+
+    public Map<String, String> getTags()
+    {
+        return tags;
+    }
+
+    public void setTags( Map<String, String> tags )
+    {
+        this.tags = tags;
     }
 }
