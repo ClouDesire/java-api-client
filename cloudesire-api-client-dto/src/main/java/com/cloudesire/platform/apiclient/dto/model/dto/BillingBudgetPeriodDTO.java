f4f6b0140e95510b85b41fbf36f976049da937ef
Matteo Giordano <Matteo.Giordano@eng.it>
2022-12-21 18:49:16 +0100

    Configure a custom period for billing budgets (#871)

---
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/BillingBudgetPeriodDTO.java
@@ -0,0 +1,65 @@
+package com.cloudesire.platform.apiclient.dto.model.dto;
+
+import com.cloudesire.platform.apiclient.dto.model.enums.CalendarPeriod;
+import io.swagger.annotations.ApiModel;
+
+import javax.validation.Valid;
+import java.util.Objects;
+
+@ApiModel( description = "Timeframe definition for a billing budget. Either a calendar or a custom period can be defined." )
+public class BillingBudgetPeriodDTO implements DTO
+{
+    private CalendarPeriod calendarPeriod;
+
+    @Valid
+    private CustomPeriodDTO customPeriod;
+
+    public BillingBudgetPeriodDTO( CalendarPeriod calendarPeriod )
+    {
+        this.calendarPeriod = calendarPeriod;
+    }
+
+    public BillingBudgetPeriodDTO( CustomPeriodDTO customPeriod )
+    {
+        this.customPeriod = customPeriod;
+    }
+
+    public BillingBudgetPeriodDTO()
+    {
+    }
+
+    public CalendarPeriod getCalendarPeriod()
+    {
+        return calendarPeriod;
+    }
+
+    public void setCalendarPeriod( CalendarPeriod calendarPeriod )
+    {
+        this.calendarPeriod = calendarPeriod;
+    }
+
+    public CustomPeriodDTO getCustomPeriod()
+    {
+        return customPeriod;
+    }
+
+    public void setCustomPeriod( CustomPeriodDTO customPeriod )
+    {
+        this.customPeriod = customPeriod;
+    }
+
+    @Override
+    public boolean equals( Object o )
+    {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+        BillingBudgetPeriodDTO that = (BillingBudgetPeriodDTO) o;
+        return calendarPeriod == that.calendarPeriod && Objects.equals( customPeriod, that.customPeriod );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( calendarPeriod, customPeriod );
+    }
+}
---
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/CustomPeriodDTO.java
@@ -0,0 +1,69 @@
+package com.cloudesire.platform.apiclient.dto.model.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@ApiModel( description = "Custom period definition, alternative to CalendarPeriod." )
+public class CustomPeriodDTO implements DTO
+{
+    @ApiModelProperty( "Custom period start date, inclusive" )
+    @NotNull
+    private LocalDate startDate;
+
+    @ApiModelProperty( "Custom period end date, exclusive. If null, period streches to now" )
+    private LocalDate endDate;
+
+    public CustomPeriodDTO( LocalDate startDate, LocalDate endDate )
+    {
+        this( startDate );
+        this.endDate = endDate;
+    }
+
+    public CustomPeriodDTO( LocalDate startDate )
+    {
+        this.startDate = startDate;
+    }
+
+    public CustomPeriodDTO()
+    {
+    }
+
+    public LocalDate getStartDate()
+    {
+        return startDate;
+    }
+
+    public void setStartDate( LocalDate startDate )
+    {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate()
+    {
+        return endDate;
+    }
+
+    public void setEndDate( LocalDate endDate )
+    {
+        this.endDate = endDate;
+    }
+
+    @Override
+    public boolean equals( Object o )
+    {
+        if ( this == o ) return true;
+        if ( o == null || getClass() != o.getClass() ) return false;
+        CustomPeriodDTO that = (CustomPeriodDTO) o;
+        return Objects.equals( startDate, that.startDate ) && Objects.equals( endDate, that.endDate );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( startDate, endDate );
+    }
+}
