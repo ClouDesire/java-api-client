1047244625337bace49a775846b18fe581ce6703
Matteo Giordano <matteo.giordano@eng.it>
2023-01-05 11:35:49 +0100

    Expose threshold notification timestamp

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/BillingBudgetThresholdDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/BillingBudgetThresholdDTO.java
@@ -7,6 +7,9 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static io.swagger.annotations.ApiModelProperty.AccessMode.READ_ONLY;
 
 @ApiModel( description = "Defines the triggering events used to generate a budget notification email." )
 public class BillingBudgetThresholdDTO implements DTO
@@ -16,6 +19,9 @@ public class BillingBudgetThresholdDTO implements DTO
     @Max( 100 )
     @NotNull
     private BigDecimal percent;
+
+    @ApiModelProperty( value = "When this threshold has been notified", accessMode = READ_ONLY )
+    private LocalDateTime notifiedAt;
 
     public BillingBudgetThresholdDTO( String percent )
     {
@@ -34,5 +40,15 @@ public class BillingBudgetThresholdDTO implements DTO
     public void setPercent( BigDecimal percent )
     {
         this.percent = percent;
+    }
+
+    public LocalDateTime getNotifiedAt()
+    {
+        return notifiedAt;
+    }
+
+    public void setNotifiedAt( LocalDateTime notifiedAt )
+    {
+        this.notifiedAt = notifiedAt;
     }
 }
