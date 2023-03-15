dff0f5ebf67c58ec665b385c253eed4378d3a135
Matteo Giordano <Matteo.Giordano@eng.it>
2023-03-15 15:57:24 +0100

    Export platform costs and earnings

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/csv/CostCsvDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/csv/CostCsvDTO.java
@@ -5,19 +5,33 @@ import com.cloudesire.platform.apiclient.dto.model.enums.LineType;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 @JsonPropertyOrder( {
-        "subscription", "type", "billingItem", "identifier", "cost"
+        "subscription", "subscriptionName", "type", "billingItem", "identifier", "description", "start", "end",
+        "purchased", "currency", "cost"
 } )
 public class CostCsvDTO implements DTO
 {
     private Integer subscription;
+
+    private String subscriptionName;
 
     private LineType type;
 
     private String billingItem;
 
     private String identifier;
+
+    private String description;
+
+    private Date start;
+
+    private Date end;
+
+    private Date purchased;
+
+    private String currency;
 
     private BigDecimal cost;
 
@@ -29,6 +43,16 @@ public class CostCsvDTO implements DTO
     public void setSubscription( Integer subscription )
     {
         this.subscription = subscription;
+    }
+
+    public String getSubscriptionName()
+    {
+        return subscriptionName;
+    }
+
+    public void setSubscriptionName( String subscriptionName )
+    {
+        this.subscriptionName = subscriptionName;
     }
 
     public LineType getType()
@@ -59,6 +83,56 @@ public class CostCsvDTO implements DTO
     public void setIdentifier( String identifier )
     {
         this.identifier = identifier;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription( String description )
+    {
+        this.description = description;
+    }
+
+    public Date getStart()
+    {
+        return start;
+    }
+
+    public void setStart( Date start )
+    {
+        this.start = start;
+    }
+
+    public Date getEnd()
+    {
+        return end;
+    }
+
+    public void setEnd( Date end )
+    {
+        this.end = end;
+    }
+
+    public Date getPurchased()
+    {
+        return purchased;
+    }
+
+    public void setPurchased( Date purchased )
+    {
+        this.purchased = purchased;
+    }
+
+    public String getCurrency()
+    {
+        return currency;
+    }
+
+    public void setCurrency( String currency )
+    {
+        this.currency = currency;
     }
 
     public BigDecimal getCost()
