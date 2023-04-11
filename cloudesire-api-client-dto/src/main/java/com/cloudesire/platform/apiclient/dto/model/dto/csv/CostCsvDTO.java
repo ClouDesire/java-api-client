d31947ba28d7e93c6312b4fe0b69e8ed135b0c5f
Matteo Giordano <Matteo.Giordano@eng.it>
2023-04-11 17:14:52 +0200

    Export platform costs and earnings (#888)

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/csv/CostCsvDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/csv/CostCsvDTO.java
@@ -2,16 +2,22 @@ package com.cloudesire.platform.apiclient.dto.model.dto.csv;
 
 import com.cloudesire.platform.apiclient.dto.model.dto.DTO;
 import com.cloudesire.platform.apiclient.dto.model.enums.LineType;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Map;
 
 @JsonPropertyOrder( {
-        "subscription", "type", "billingItem", "identifier", "cost"
+        "subscription", "subscriptionName", "type", "billingItem", "identifier", "description", "start", "end",
+        "purchased", "nominee", "operator", "reseller", "quantity", "currency", "cost", "platformCost"
 } )
 public class CostCsvDTO implements DTO
 {
     private Integer subscription;
+
+    private String subscriptionName;
 
     private LineType type;
 
@@ -19,7 +25,29 @@ public class CostCsvDTO implements DTO
 
     private String identifier;
 
+    private String description;
+
+    private Date start;
+
+    private Date end;
+
+    private String nominee;
+
+    private String operator;
+
+    private String reseller;
+
+    private Date purchased;
+
+    private BigDecimal quantity;
+
+    private String currency;
+
     private BigDecimal cost;
+
+    private BigDecimal platformCost;
+
+    private Map<String, String> metadata;
 
     public Integer getSubscription()
     {
@@ -29,6 +57,16 @@ public class CostCsvDTO implements DTO
     public void setSubscription( Integer subscription )
     {
         this.subscription = subscription;
+    }
+
+    public String getSubscriptionName()
+    {
+        return subscriptionName;
+    }
+
+    public void setSubscriptionName( String subscriptionName )
+    {
+        this.subscriptionName = subscriptionName;
     }
 
     public LineType getType()
@@ -61,6 +99,96 @@ public class CostCsvDTO implements DTO
         this.identifier = identifier;
     }
 
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription( String description )
+    {
+        this.description = description;
+    }
+
+    public Date getStart()
+    {
+        return start;
+    }
+
+    public void setStart( Date start )
+    {
+        this.start = start;
+    }
+
+    public Date getEnd()
+    {
+        return end;
+    }
+
+    public void setEnd( Date end )
+    {
+        this.end = end;
+    }
+
+    public String getNominee()
+    {
+        return nominee;
+    }
+
+    public void setNominee( String nominee )
+    {
+        this.nominee = nominee;
+    }
+
+    public String getOperator()
+    {
+        return operator;
+    }
+
+    public void setOperator( String operator )
+    {
+        this.operator = operator;
+    }
+
+    public String getReseller()
+    {
+        return reseller;
+    }
+
+    public void setReseller( String reseller )
+    {
+        this.reseller = reseller;
+    }
+
+    public Date getPurchased()
+    {
+        return purchased;
+    }
+
+    public void setPurchased( Date purchased )
+    {
+        this.purchased = purchased;
+    }
+
+    public BigDecimal getQuantity()
+    {
+        return quantity;
+    }
+
+    public void setQuantity( BigDecimal quantity )
+    {
+        this.quantity = quantity;
+    }
+
+    public String getCurrency()
+    {
+        return currency;
+    }
+
+    public void setCurrency( String currency )
+    {
+        this.currency = currency;
+    }
+
     public BigDecimal getCost()
     {
         return cost;
@@ -69,5 +197,26 @@ public class CostCsvDTO implements DTO
     public void setCost( BigDecimal cost )
     {
         this.cost = cost;
+    }
+
+    public BigDecimal getPlatformCost()
+    {
+        return platformCost;
+    }
+
+    public void setPlatformCost( BigDecimal platformCost )
+    {
+        this.platformCost = platformCost;
+    }
+
+    @JsonAnyGetter
+    public Map<String, String> getMetadata()
+    {
+        return metadata;
+    }
+
+    public void setMetadata( Map<String, String> metadata )
+    {
+        this.metadata = metadata;
     }
 }
