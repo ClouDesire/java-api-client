201e8ce7842c67ea9aef1755e6b88edb21ef82ef
Matteo Giordano <Matteo.Giordano@eng.it>
2023-05-22 17:02:15 +0200

    Export a Cart via CSV

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CartApi.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/api/CartApi.java
@@ -3,15 +3,18 @@ package com.cloudesire.platform.apiclient.api;
 import com.cloudesire.platform.apiclient.dto.model.dto.CartDTO;
 import com.cloudesire.platform.apiclient.query.CartQuery;
 import com.cloudesire.platform.apiclient.query.ResellerCatalogQuery;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 import retrofit2.http.QueryMap;
+import retrofit2.http.Streaming;
 
 import java.util.List;
 import java.util.Map;
@@ -39,6 +42,11 @@ public interface CartApi
 
     @GET( "cart/{id}" )
     Call<CartDTO> get( @Path( "id" ) int id );
+
+    @GET( "cart/{id}" )
+    @Headers( { "Accept:text/csv" } )
+    @Streaming
+    Call<ResponseBody> getCsv( @Path( "id" ) int id );
 
     @PUT( "cart/{id}" )
     Call<CartDTO> update( @Path( "id" ) int id, @Body CartDTO cart );
