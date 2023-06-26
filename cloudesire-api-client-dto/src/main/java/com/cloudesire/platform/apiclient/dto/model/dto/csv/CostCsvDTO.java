5ea5d8dabe828b18ee250b8cea7761790c5c17bb
Matteo Giordano <Matteo.Giordano@eng.it>
2023-06-26 14:30:51 +0200

    Export all channel costs for admin via CSV (#936)

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/csv/CostCsvDTO.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/dto/csv/CostCsvDTO.java
@@ -4,18 +4,13 @@ import com.cloudesire.platform.apiclient.dto.model.dto.DTO;
 import com.cloudesire.platform.apiclient.dto.model.enums.OrderType;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 
-@JsonPropertyOrder( {
-        "subscription", "productVersion", "subscriptionName", "orderType", "orders", "typeCode", "description",
-        "start", "end", "purchased", "nominee", "operator", "reseller", "quantity", "currency", "cost", "platformCost"
-} )
-public class CostCsvDTO implements DTO
+public abstract class CostCsvDTO implements DTO
 {
     @JsonProperty( "Subscription ID" )
     private Integer subscription;
@@ -44,12 +39,6 @@ public class CostCsvDTO implements DTO
     @JsonProperty( "End date" )
     private Date end;
 
-    @JsonProperty( "Company" )
-    private String nominee;
-
-    @JsonProperty( "Buyer" )
-    private String operator;
-
     @JsonProperty( "Reseller" )
     private String reseller;
 
@@ -61,12 +50,6 @@ public class CostCsvDTO implements DTO
 
     @JsonProperty( "Currency" )
     private String currency;
-
-    @JsonProperty( "Revenues" )
-    private BigDecimal cost;
-
-    @JsonProperty( "Costs" )
-    private BigDecimal platformCost;
 
     private Map<String, String> metadata;
 
@@ -160,26 +143,6 @@ public class CostCsvDTO implements DTO
         this.end = end;
     }
 
-    public String getNominee()
-    {
-        return nominee;
-    }
-
-    public void setNominee( String nominee )
-    {
-        this.nominee = nominee;
-    }
-
-    public String getOperator()
-    {
-        return operator;
-    }
-
-    public void setOperator( String operator )
-    {
-        this.operator = operator;
-    }
-
     public String getReseller()
     {
         return reseller;
@@ -218,26 +181,6 @@ public class CostCsvDTO implements DTO
     public void setCurrency( String currency )
     {
         this.currency = currency;
-    }
-
-    public BigDecimal getCost()
-    {
-        return cost;
-    }
-
-    public void setCost( BigDecimal cost )
-    {
-        this.cost = cost;
-    }
-
-    public BigDecimal getPlatformCost()
-    {
-        return platformCost;
-    }
-
-    public void setPlatformCost( BigDecimal platformCost )
-    {
-        this.platformCost = platformCost;
     }
 
     @JsonAnyGetter
