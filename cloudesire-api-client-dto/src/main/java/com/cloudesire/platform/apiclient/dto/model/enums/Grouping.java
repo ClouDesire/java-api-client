537136d3bb80bcb9c8179d249e3d508fe997c08f
Matteo Giordano <Matteo.Giordano@eng.it>
2023-09-29 12:57:45 +0200

    Aggregate costs by month

--- cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/Grouping.java
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/enums/Grouping.java
@@ -2,5 +2,17 @@ package com.cloudesire.platform.apiclient.dto.model.enums;
 
 public enum Grouping
 {
-    DAY_OF_MONTH
+    DAY_OF_MONTH( "day" ), MONTH( "month" );
+
+    private final String field;
+
+    Grouping( String field )
+    {
+        this.field = field;
+    }
+
+    public String getField()
+    {
+        return field;
+    }
 }
