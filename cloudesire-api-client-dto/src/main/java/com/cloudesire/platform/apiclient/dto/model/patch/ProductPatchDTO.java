cb1ff45906ffb9abe8a3e8a3ca61cd17628dd99e
Matteo Giordano <Matteo.Giordano@eng.it>
2023-11-23 13:57:50 +0100

    Move currency conversion to product (#975)

---
+++ cloudesire-api-client-dto/src/main/java/com/cloudesire/platform/apiclient/dto/model/patch/ProductPatchDTO.java
@@ -0,0 +1,80 @@
+package com.cloudesire.platform.apiclient.dto.model.patch;
+
+import com.cloudesire.platform.apiclient.dto.model.dto.DTO;
+import com.fasterxml.jackson.databind.EnumNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.EnumNaming;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.Map;
+
+public class ProductPatchDTO implements DTO
+{
+    @NotNull
+    private Action action;
+
+    @ApiModelProperty( "Conversion factors from foreign currencies" )
+    private Map<String, BigDecimal> currencyConversion;
+
+    public static ProductPatchDTO enable()
+    {
+        return new ProductPatchDTO( Action.ENABLED );
+    }
+
+    public static ProductPatchDTO disable()
+    {
+        return new ProductPatchDTO( Action.DISABLED );
+    }
+
+    public static ProductPatchDTO approve()
+    {
+        return new ProductPatchDTO( Action.APPROVE );
+    }
+
+    public static ProductPatchDTO saveCurrencyConversion( Map<String, BigDecimal> currencyConversion )
+    {
+        return new ProductPatchDTO( Action.SAVE_CURRENCY_CONVERSION ).setCurrencyConversion( currencyConversion );
+    }
+
+    private ProductPatchDTO( Action action )
+    {
+        this.action = action;
+    }
+
+    public ProductPatchDTO()
+    {
+    }
+
+    public Action getAction()
+    {
+        return action;
+    }
+
+    public ProductPatchDTO setAction( Action action )
+    {
+        this.action = action;
+        return this;
+    }
+
+    public Map<String, BigDecimal> getCurrencyConversion()
+    {
+        return currencyConversion;
+    }
+
+    public ProductPatchDTO setCurrencyConversion( Map<String, BigDecimal> currencyConversion )
+    {
+        this.currencyConversion = currencyConversion;
+        return this;
+    }
+
+    @EnumNaming( EnumNamingStrategies.CamelCaseStrategy.class )
+    public enum Action
+    {
+        ENABLED,
+        DISABLED,
+        APPROVE,
+        @ApiModelProperty( "Save currency conversion factors" )
+        SAVE_CURRENCY_CONVERSION;
+    }
+}
