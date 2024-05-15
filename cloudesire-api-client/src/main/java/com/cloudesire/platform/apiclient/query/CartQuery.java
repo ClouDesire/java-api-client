f89f9a2a32271986f5ef6fc2b349d08004cde1c4
Federico Cuozzo <Federico.Cuozzo@eng.it>
2024-05-15 15:54:02 +0200

    feat: fuzzy search by name or notes for Cart

--- cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/query/CartQuery.java
+++ cloudesire-api-client/src/main/java/com/cloudesire/platform/apiclient/query/CartQuery.java
@@ -6,6 +6,7 @@ public class CartQuery extends BaseQuery
     private static final String USER_COMPANY = "userCompanyId";
     private static final String RESELLER = "resellerId";
     private static final String CHECKED_OUT = "checkedOut";
+    private static final String SEARCH = "search";
 
     public CartQuery setPageRequest( PageRequestQuery pageRequestQuery )
     {
@@ -34,6 +35,12 @@ public class CartQuery extends BaseQuery
     public CartQuery setCheckedOut( boolean checkedOut )
     {
         put( CHECKED_OUT, checkedOut );
+        return this;
+    }
+
+    public CartQuery setSearch( String search )
+    {
+        put( SEARCH, search );
         return this;
     }
 }
